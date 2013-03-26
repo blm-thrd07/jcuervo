9bf48853635199f685c7fe2b60494ba07eec1371
Oscar <oscarolbe@hotmail.com>
2013-03-25 22:45:22 -0600

    cambio controlle

--- protected/controllers/AppController.php
+++ protected/controllers/AppController.php
@@ -9,7 +9,7 @@ class AppController extends Controller
   {
     return array(
       'accessControl', // perform access control for CRUD operations
-      'postOnly + delete', // we only allow deletion via POST request
+      'postOnly + delete', // we only allow deletion via POST request s
     );
   }
 
