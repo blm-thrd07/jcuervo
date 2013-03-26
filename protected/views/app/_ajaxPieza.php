5cd8c87d280ac93fe316b3b7bf585a9d4794b880
System Administrator <root@MacBook-Pro-de-Jair-2.local>
2013-03-25 22:57:01 -0600

    bolblb

--- protected/views/app/_ajaxPieza.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-foreach ($piezas as $key => $value) {
-	//echo $value->url." ";
-	//vecho CHtml::ajaxLink($value->url, CController::CreateUrl('app/UpdatePieza'),array('id'=>uniqId(), 'live'=>false,/*'update' => '#data',*/'type'=>'POST','data'=>'pieza_id='.$value->id.'&accion=INSERTAR'))." ";
-	echo CHtml::link($value->url, "#", array('class'=>"insertar",'name'=>$value->id))." "; 
-
-}
