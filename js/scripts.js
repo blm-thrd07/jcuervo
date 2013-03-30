f5a0558f0d3e8367128ef76ad2e84fbcb1b99d4c
Jair <dev.lnx1337@gmail.com>
2013-03-30 10:45:45 -0600

    upload avatar image

--- js/scripts.js
+++ js/scripts.js
@@ -188,7 +188,7 @@
      $.ajax({
         type: 'POST',
         url: "https://apps.t2omedia.com.mx/php2/jcuervo/index.php/avatars/update/"+iU,
-        data: dataUrl,
+        data: "avatarImg="dataUrl,
         success: function(data){
            alert(data);
         }
