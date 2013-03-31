f1ea42e4287d58977b5231a47f845367d955dcea
Jair <dev.lnx1337@gmail.com>
2013-03-31 00:52:50 -0600

    facebook

--- protected/config/main.php
+++ protected/config/main.php
@@ -88,7 +88,7 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		// this is used in contact page
-		'facebook'=>''
+		'facebook'=>'',
 		'adminEmail'=>'webmaster@example.com',
 	),
 );
