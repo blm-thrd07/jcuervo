744d406a44e753ffda093ce342286dd871add5d6
Jair <dev.lnx1337@gmail.com>
2013-03-31 17:44:50 -0600

    Login

--- protected/views/app/login.php
+++ protected/views/app/login.php
@@ -1,16 +1,16 @@
-<?
 
-//echo "<a  target='_top' href='".$loginUrl."' target='_self'>Login</a>";
-
-?>
+<a  id="login">Login</a>
 
 <script>
   var oauth_url = 'https://www.facebook.com/dialog/oauth/';
   oauth_url += '?client_id=342733185828640';
   oauth_url += '&redirect_uri=' + encodeURIComponent('http://apps.facebook.com/lnxjair');
   oauth_url += '&scope=email,read_stream,user_likes,publish_actions,publish_stream'
-
+  
+document.getElementById("login").onclick = function() {
   window.top.location = oauth_url;
+  }
+
 </script>
 
 <?
