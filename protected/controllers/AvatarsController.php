bcd56f8f5085ecb3afde5039d63d0aae4cab20fa
Jair <dev.lnx1337@gmail.com>
2013-03-31 20:38:16 -0600

    cih

--- protected/controllers/AvatarsController.php
+++ protected/controllers/AvatarsController.php
@@ -27,7 +27,7 @@ class AvatarsController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view','UpdatePieza','update'),
+				'actions'=>array('index','view','UpdatePieza'),
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
