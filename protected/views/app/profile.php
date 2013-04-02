f607c7a7eedf488d0a81360b9c7745a2780b1ca5
Jair <dev.lnx1337@gmail.com>
2013-04-01 23:15:06 -0600

    front

--- protected/views/app/profile.php
+++ protected/views/app/profile.php
@@ -4,8 +4,8 @@
         <div id="actions"><a href="#" class="btn"><i class="icon-edit"></i> Editar</a></div>
 </section>
       <section id="panelContent">
-        <h2>Mis Memes</h2>
-        <div class="tabs"><a href="destacados.html">Destacados</a><a id="mismemes" href="" class="selectedTab">Mis Memes</a><a href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/misamigos">De mis amigos</a><a  id="categoria" href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/categoria">Por categoría</a></div>
+     <h2>Mis Memes</h2>
+        <div class="tabs"><a href="destacados.html">Destacados</a><a href="mismemes.html" class="selectedTab">Mis Memes</a><a href="misamigos.html">De mis amigos</a><a href="categoria.html">Por categoría</a></div>
         <div class="memeThumbs">
           <div class="itemMeme"><a href="#c" class="itemAction"><i class="icon-plus-sign"></i>Crea Nuevo Meme</a></div>
           <div class="itemMeme"><a href="#a"><img src="http://placehold.it/640x480.png"></a></div>
@@ -15,7 +15,6 @@
           <div class="itemMeme"><a href="#a"><img src="http://placehold.it/640x480.png"></a></div>
         </div>
         <div class="pager"><a href="#" class="btn"><i class="icon-chevron-left"></i></a><a href="#" class="btn"><i class="icon-chevron-right"></i></a></div>
-    
 
       </section>
 <?
