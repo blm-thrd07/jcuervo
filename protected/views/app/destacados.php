ae1c2d9e0623fbb7629fad4f10ada1bf33b40987
Jair <dev.lnx1337@gmail.com>
2013-04-02 08:15:45 -0600

    front

--- protected/views/app/destacados.php
+++ protected/views/app/destacados.php
@@ -1,5 +1,5 @@
 <h2>Destacados</h2>
-        <div class="tabs"><a id="des" class="selectedTab menu" href="#" >Destacados</a><a id="mismemes" href="#" class="menu">Mis Memes</a><a  id="misamigos"  class="menu" href="">De mis amigos</a><a id="categoria" class="menu" href="">Por categoría</a></div>
+        <div class="tabs"><a id="Destacados" class="selectedTab menu" href="#" >Destacados</a><a id="mismemes" href="#" class="menu">Mis Memes</a><a  id="misamigos"  class="menu" href="">De mis amigos</a><a id="categoria" class="menu" href="">Por categoría</a></div>
         <div class="memeThumbs">
           <div class="itemMeme"><a href="detalle.html"><img src="http://placehold.it/640x480.png"></a>
             <div><a href="amigo.html"><img src="http://placehold.it/32x32.png"></a></div>
