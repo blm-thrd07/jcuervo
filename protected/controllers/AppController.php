b785b089012fe3a66fc94f26e097ff4768860ce3
Jair <dev.lnx1337@gmail.com>
2013-04-02 08:19:18 -0600

    front

--- protected/controllers/AppController.php
+++ protected/controllers/AppController.php
@@ -20,11 +20,11 @@ class AppController extends Controller
   {
     return array(
       array('allow',  // allow all users to perform 'index' and 'view' actions
-        'actions'=>array('view','Logout','login','Categoria'),
+        'actions'=>array('view','Logout','login'),
         'users'=>array('*'),
       ),
       array('allow', // allow authenticated user to perform 'create' and 'update' actions
-        'actions'=>array('create','update','create','profile','UpdatePieza','CrearAvatar','UpdateTipoPieza','MisMemes','MisAmigos'),
+        'actions'=>array('create','update','create','profile','UpdatePieza','CrearAvatar','UpdateTipoPieza','MisMemes','MisAmigos','Categoria','Destacados'),
         'users'=>array('@'),
       ),
       array('allow', // allow admin user to perform 'admin' and 'delete' actions
