eab5db7728ac2bfb19412eaf7e18e70932211f1a
Jair <dev.lnx1337@gmail.com>
2013-04-01 19:09:02 -0600

    front ajax

--- protected/views/app/profile.php
+++ protected/views/app/profile.php
@@ -5,10 +5,19 @@
       </section>
       
       <section id="panelContent">
-        
         <h2>Mis Memes</h2>
-        <div class="tabs"><a id="destacados" href="destacados.html">Destacados</a><a href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/mismemes" class="selectedTab">Mis Memes</a><a href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/misamigos">De mis amigos</a><a href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/categoria">Por categoría</a></div>
-        
+        <div class="tabs"><a href="destacados.html">Destacados</a><a href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/mismemes" class="selectedTab">Mis Memes</a><a href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/misamigos">De mis amigos</a><a  id="categoria" href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/categoria">Por categoría</a></div>
+        <div class="memeThumbs">
+          <div class="itemMeme"><a href="#c" class="itemAction"><i class="icon-plus-sign"></i>Crea Nuevo Meme</a></div>
+          <div class="itemMeme"><a href="#a"><img src="http://placehold.it/640x480.png"></a></div>
+          <div class="itemMeme"><a href="#a"><img src="http://placehold.it/640x480.png"></a></div>
+          <div class="itemMeme"><a href="#a"><img src="http://placehold.it/640x480.png"></a></div>
+          <div class="itemMeme"><a href="#a"><img src="http://placehold.it/640x480.png"></a></div>
+          <div class="itemMeme"><a href="#a"><img src="http://placehold.it/640x480.png"></a></div>
+        </div>
+        <div class="pager"><a href="#" class="btn"><i class="icon-chevron-left"></i></a><a href="#" class="btn"><i class="icon-chevron-right"></i></a></div>
+    
+
       </section>
 
 
@@ -429,7 +438,15 @@ Yii::app()->getClientScript()->registerScript('registrar', '
   };
 
 $("#destacados").click(function(){
-  alert("puto");
+$.ajax({
+          type: "GET",
+          url: "http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/categoria",
+          data: avatarJson,
+          success: function(data){
+              $("#panelContent").html(data);
+          }
+        });
+
 });
 
 
