cbb5bd518d72730d249eee53307275f1d341c8e5
Oscar <oscarolbe@hotmail.com>
2013-04-03 13:39:45 -0600

    link comics create

--- protected/views/app/profile.php
+++ protected/views/app/profile.php
@@ -7,7 +7,7 @@
      <h2>Mis Memes</h2>
         <div class="tabs"><a id="dest" class="menu" href="#" >Destacados</a><a id="mismemes" href="#" class="selectedTab menu">Mis Memes</a><a  id="misamigos"  class="menu" href="#">De mis amigos</a><a id="categoria" class="menu" href="#">Por categoría</a></div>
         <div class="memeThumbs">
-          <div class="itemMeme"><a href="#c" class="itemAction"><i class="icon-plus-sign"></i>Crea Nuevo Meme</a></div>
+          <div class="itemMeme"><a href="<?php echo CController::CreateUrl('Comics/create'); ?>" class="itemAction"><i class="icon-plus-sign"></i>Crea Nuevo Meme</a></div>
               <? 
               if(is_array($json['avatar']['comics'])){
                     foreach ($json['avatar']['comics'] as $key => $value) {
