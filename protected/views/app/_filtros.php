74b0040409481bee4280c06767102928901bb7b3
Jair <dev.lnx1337@gmail.com>
2013-04-03 03:09:52 -0600

    cambio

--- protected/views/app/_filtros.php
+++ protected/views/app/_filtros.php
@@ -1,5 +1,5 @@
 
-<? print_r($destacados); ?>
+<? echo $destacados->Comic->imagen; ?>
 <div class="itemMeme"><a href="detalle.html"><img src="http://placehold.it/640x480.png"></a>
           <div><a href="amigo.html"><img src="http://placehold.it/32x32.png"></a></div>
           </div>
