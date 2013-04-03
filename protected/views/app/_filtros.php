1ffce40f7538823a56684a90d6107e6eb55bef4a
Jair <dev.lnx1337@gmail.com>
2013-04-03 03:19:36 -0600

    ca

--- protected/views/app/_filtros.php
+++ protected/views/app/_filtros.php
@@ -2,7 +2,7 @@
 <?
 $cantidad_resultados=count($resultado);
 for($i=0;$i<$cantidad_resultados;$i++){
-echo '<div class="itemMeme"><a href="detalle.html">'.CHtml::image(Yii::app()->request->baseUrl."/Comics/".$resultado[$i]->Comic->imagen).'</a><div><a href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/'.$destacados[$i]->Usuario->id_facebook.'">'.CHtml::image('https://graph.facebook.com/'.$destacados[$i]->Usuario->id_facebook.'/picture').'</a></div>
+echo '<div class="itemMeme"><a href="detalle.html">'.CHtml::image(Yii::app()->request->baseUrl."/Comics/".$resultado[$i]->Comic->imagen).'</a><div><a href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/'.$resultado[$i]->Usuario->id_facebook.'">'.CHtml::image('https://graph.facebook.com/'.$resultado[$i]->Usuario->id_facebook.'/picture').'</a></div>
              </div>';
 }
 ?>
