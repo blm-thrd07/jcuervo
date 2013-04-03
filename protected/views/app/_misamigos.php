58b7e01cf970440598c44d2c53c41ca68411a767
Jair <dev.lnx1337@gmail.com>
2013-04-03 01:31:23 -0600

    otro

--- protected/views/app/_misamigos.php
+++ protected/views/app/_misamigos.php
@@ -2,13 +2,9 @@
         <h2>Memes de mis amigos</h2>
         <div class="tabs"><a id="dest" class="menu" href="#" >Destacados</a><a id="mismemes" href="#" class="menu">Mis Memes</a><a  id="misamigos"  class="selectedTab menu" href="">De mis amigos</a><a id="categoria" class="menu" href="">Por categoría</a></div>
         <div class="memeThumbs">
-          
-          <div class="itemMeme"><a href="detalle.html"><img src="http://placehold.it/640x480.png"></a>
-            <div><a href="amigo.html"><img src="http://placehold.it/32x32.png"></a></div>
-          </div>
           <?
           foreach ($comicsAmigos['comicsAmigos'] as $key => $value) {
-            echo '<div class="itemMeme"><a href="detalle.html">'.CHtml::image(Yii::app()->request->baseUrl."/Comics/".$value['imagen']).'</a><div><a href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/"'.$value['idFb'].'>'.CHtml::image('https://graph.facebook.com/'.$value['idFb'].'/picture').'</a></div>
+            echo '<div class="itemMeme"><a href="detalle.html">'.CHtml::image(Yii::app()->request->baseUrl."/Comics/".$value['imagen']).'</a><div><a href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/'.$value['idFb'].'">'.CHtml::image('https://graph.facebook.com/'.$value['idFb'].'/picture').'</a></div>
              </div>';
           }                   
           ?>
