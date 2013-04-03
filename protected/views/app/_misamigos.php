fddf2c1756e6513d76bc0f2a3999ac483a28c52d
Jair <dev.lnx1337@gmail.com>
2013-04-03 01:33:47 -0600

    otro

--- protected/views/app/_misamigos.php
+++ protected/views/app/_misamigos.php
@@ -3,9 +3,7 @@
         <div class="tabs"><a id="dest" class="menu" href="#" >Destacados</a><a id="mismemes" href="#" class="menu">Mis Memes</a><a  id="misamigos"  class="selectedTab menu" href="">De mis amigos</a><a id="categoria" class="menu" href="">Por categoría</a></div>
         <div class="memeThumbs">
           <?
-          foreach ($comicsAmigos['comicsAmigos'] as $key => $value) {
-            
-            for($i=0;$i<100;$i++)
+          foreach ($comicsAmigos['comicsAmigos'] as $key => $value){
               echo '<div class="itemMeme"><a href="detalle.html">'.CHtml::image(Yii::app()->request->baseUrl."/Comics/".$value['imagen']).'</a><div><a href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/'.$value['idFb'].'">'.CHtml::image('https://graph.facebook.com/'.$value['idFb'].'/picture').'</a></div>
              </div>';
           }                   
