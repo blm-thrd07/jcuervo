2076568ef43dbeb00ead697a8d2772701feeb764
Jair <dev.lnx1337@gmail.com>
2013-04-02 18:19:38 -0600

    bla

--- protected/models/Amigos.php
+++ protected/models/Amigos.php
@@ -106,10 +106,15 @@ public function getAmigosComics(){
     $response= Amigos::model()->findAll(array('condition'=>'usuarios_id=:uid','params'=>array(':uid'=> Yii::app()->session['usuario_id'])));   
      //     print_r($response[0]->amigo->Comics[0]->Comic);
     $response=Amigos::model()->with('amigo.Comics.Comic.Coments')->findAll(array('condition'=>'usuarios_id=:uid','params'=>array(':uid'=> Yii::app()->session['usuario_id'])));
-
+/*
 for ($i=0; $i <count($response) ; $i++) { 
-	echo $response[$i]->amigo->Comics[0]->tbl_comics_id;
+	echo $response[$i]->amigo->Comics;
+
+	print_r(expression)
 }
+*/
+
+print_r($response[0]->amigo[0]);
    //print_r($response);
      //print_r($response[1]->amigo->Comics);
 
