131c7ad805f067233f6598fbb9adc808ab783711
Jair <dev.lnx1337@gmail.com>
2013-04-02 23:52:31 -0600

    cambio

--- protected/views/app/_mismemes.php
+++ protected/views/app/_mismemes.php
@@ -1,22 +1,17 @@
         <h2>Mis Memes</h2>
         <div class="tabs"><a id="dest" class="menu" href="#" >Destacados</a><a id="mismemes" href="#" class="selectedTab menu">Mis Memes</a><a  id="misamigos"  class="menu" href="">De mis amigos</a><a id="categoria" class="menu" href="">Por categoría</a></div>
         <div class="memeThumbs">
-          <div class="itemMeme"><a href="#c" class="itemAction"><i class="icon-plus-sign"></i>Crea Nuevo Meme</a></div>
+        <div class="itemMeme"><a href="#c" class="itemAction"><i class="icon-plus-sign"></i>Crea Nuevo Meme</a></div>
           
-          <div class="itemMeme"><a href="#a"><img src="http://placehold.it/640x480.png"></a></div>
-          <div class="itemMeme"><a href="#a"><img src="http://placehold.it/640x480.png"></a></div>
-          <div class="itemMeme"><a href="#a"><img src="http://placehold.it/640x480.png"></a></div>
-          <div class="itemMeme"><a href="#a"><img src="http://placehold.it/640x480.png"></a></div>
-          <div class="itemMeme"><a href="#a"><img src="http://placehold.it/640x480.png"></a></div>
-        
+        <? foreach ($comics['comics'] as $key => $value) {
+
+             echo '<div class="itemMeme"><a href="#a">'.CHtml::image(Yii::app()->request->baseUrl."/Comics/".$value['imagen'],"Comic").'</a></div>';
+           }       
+        ?>
 
         </div>
         <div class="pager"><a href="#" class="btn"><i class="icon-chevron-left"></i></a><a href="#" class="btn"><i class="icon-chevron-right"></i></a></div>
     
-    <? print_r(json_encode($comics)); 
-
-foreach ($comics['comics'] as $key => $value) {
-
-         echo $value['imagen'];
-}
+         <? print_r(json_encode($comics)); 
+         
     ?>
