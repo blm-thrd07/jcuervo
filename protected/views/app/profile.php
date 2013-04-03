4dc9c87843fa0b03266f2e97fbdf3d08bde25f5b
Jair <dev.lnx1337@gmail.com>
2013-04-03 01:47:05 -0600

    otro

--- protected/views/app/profile.php
+++ protected/views/app/profile.php
@@ -5,7 +5,7 @@
 </section>
 <section id="panelContent">
      <h2>Mis Memes</h2>
-     <?print_r($json['comics']);?>
+     <?print_r($json['avatar']['comics']);?>
         <div class="tabs"><a id="dest" class="menu" href="#" >Destacados</a><a id="mismemes" href="#" class="selectedTab menu">Mis Memes</a><a  id="misamigos"  class="menu" href="#">De mis amigos</a><a id="categoria" class="menu" href="#">Por categoría</a></div>
         <div class="memeThumbs">
           <div class="itemMeme"><a href="#c" class="itemAction"><i class="icon-plus-sign"></i>Crea Nuevo Meme</a></div>
