10e827e4c58c57da823d14327918adcbf30bd2eb
Jair <dev.lnx1337@gmail.com>
2013-04-03 01:06:41 -0600

    otr

--- protected/views/app/_misamigos.php
+++ protected/views/app/_misamigos.php
@@ -14,10 +14,10 @@
         
         foreach ($comicsAmigos['comicsAmigos']['comics'] as $key => $value) {
           
-      
+      /*
             echo '<div class="itemMeme"><a href="detalle.html">'.CHtml::image(Yii::app()->request->baseUrl."/Comics/".$value['imagen']).'</a><div><a href="amigo.html">'.CHtml::image('https://graph.facebook.com/'.$value['idFb'].'/picture').'</a></div>
              </div>';
-
+*/
         
              
            } 
