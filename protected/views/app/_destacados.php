83d94b59ab4cd26fc061c98bd71cd6177db8b411
Jair <dev.lnx1337@gmail.com>
2013-04-04 22:08:59 -0600

    otros

--- protected/views/app/_destacados.php
+++ protected/views/app/_destacados.php
@@ -1,5 +1,5 @@
 <h2>Destacados</h2>
-        <div class="tabs"><a id="dest" class="selectedTab subcat" href="#" >Destacados</a><a id="mismemes" href="#" class="menu">Mis Memes</a><a  id="misamigos"  class="menu" href="">De mis amigos</a><a id="categoria" class="menu" href="">Por categoría</a></div>
+        <div class="tabs"><a id="catjoscuer" class="subcat selectedTab" href="#" >Destacados</a><a id="mismemes" href="#" class="menu">Mis Memes</a><a  id="misamigos"  class="menu" href="">De mis amigos</a><a id="categoria" class="menu" href="">Por categoría</a></div>
          <div class="memeThumbs response"> 
          </div>        
       
