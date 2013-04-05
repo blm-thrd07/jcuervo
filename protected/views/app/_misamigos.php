528d401868945ce70dbcfc3ba3dbc6bd80b32bd6
Jair <dev.lnx1337@gmail.com>
2013-04-04 22:53:40 -0600

    update

--- protected/views/app/_misamigos.php
+++ protected/views/app/_misamigos.php
@@ -1,6 +1,6 @@
 
         <h2>Memes de mis amigos</h2>
-        <div class="tabs"><a id="dest" class="menu" href="#" >Destacados</a><a id="mismemes" href="#" class="menu">Mis Memes</a><a  id="misamigos"  class="selectedTab menu" href="">De mis amigos</a><a id="categoria" class="menu" href="">Por categoría</a></div>
+        <div class="tabs"><a id="mismemes" href="#" class="menu">Mis Memes</a><a  id="misamigos"  class="selectedTab menu" href="">De mis amigos</a><a id="categoria" class="menu" href="">Por categoría</a></div>
         <div class="memeThumbs">
           <? if(count($comicsAmigos)==0){?>
               <article id="noFriends">
