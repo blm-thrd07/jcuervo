105040a5c931c78228be7982307b860bad092628
Oscar <oscarolbe@hotmail.com>
2013-04-05 11:10:52 -0600

    oitro

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 (function() {
 
 //navigation menu
@@ -26,6 +31,43 @@
           });
         return false;
   });
+
+  
+$(document).ready(function() {
+    console.log('ok go');
+    $(".js-tabEngine").easytabs({
+      animate: false,
+      tabActiveClass: 'selected',
+      updateHash: false
+    });
+    $(".js-slides-1, .js-slides-2, .js-slides-3, .js-slides-4, .js-slides-5, .js-slides-6").bxSlider({
+      startingSlide: 1,
+      pager: false,
+      controls: true,
+      nextText: '→',
+      prevText: '←'
+    });
+    $("a.bx-prev, a.bx-next").bind("click", function() {
+      return setTimeout((function() {
+        $(window).trigger("scroll");
+        return console.log('yeah');
+      }), 600);
+    });
+    $(".js-lightbox").fancybox({
+      maxWidth: 810,
+      maxHeight: 600,
+      fitToView: false,
+      width: "70%",
+      height: "70%",
+      autoSize: false,
+      closeClick: false,
+      openEffect: "none",
+      closeEffect: "none"
+    });
+    return layerPersonaje.draw();
+  });
+
+  $(document).on('ready', init);
 
   
 
