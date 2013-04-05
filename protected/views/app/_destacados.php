e79c44b81b8d09de77b036fec6f4c089bd9c40d4
Jair <dev.lnx1337@gmail.com>
2013-04-04 22:27:15 -0600

    otro

--- protected/views/app/_destacados.php
+++ protected/views/app/_destacados.php
@@ -2,14 +2,11 @@
         <div class="tabs"><a id="dest" class="menu selectedTab" href="#" >Destacados</a><a id="mismemes" href="#" class="menu">Mis Memes</a><a  id="misamigos"  class="menu" href="">De mis amigos</a><a id="categoria" class="menu" href="">Por categoría</a></div>
          <div class="memeThumbs"> 
          <? 
-        if(is_array($comics)){
-         if(count($comics)!=0){
-           foreach ($comics['comics'] as $key => $value) {
-             echo '<div class="itemMeme"><a href="#a">'.CHtml::image(Yii::app()->request->baseUrl."/Comics/".$value['imagen'],"Comic").'</a></div>';
-             } 
-           }
-
-         }        
+            $cantidad_resultados=count($resultado);
+            for($i=0;$i<$cantidad_resultados;$i++){
+                  echo '<div class="itemMeme"><a href="detalle.html">'.CHtml::image(Yii::app()->request->baseUrl."/Comics/".$resultado[$i]->Comic->imagen).'</a><div><a href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/'.$resultado[$i]->Usuario->id_facebook.'">'.CHtml::image('https://graph.facebook.com/'.$resultado[$i]->Usuario->id_facebook.'/picture').'</a></div>
+             </div>';
+            }  
         ?>
          </div>        
         <div class="pager"><a href="#" class="btn"><i class="icon-chevron-left"></i></a><a href="#" class="btn"><i class="icon-chevron-right"></i></a></div>
