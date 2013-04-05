8bf07e956d81eeeb2700ff342b1b67c9f5da7272
Jair <dev.lnx1337@gmail.com>
2013-04-04 22:55:33 -0600

    has

--- protected/views/app/profile.php
+++ protected/views/app/profile.php
@@ -5,7 +5,7 @@
 </section>
 <section id="panelContent">
      <h2>Mis Memes</h2>
-        <div class="tabs"><a id="dest" class="menu" href="#" >Destacados</a><a id="mismemes" href="#" class="selectedTab menu">Mis Memes</a><a  id="misamigos"  class="menu" href="#">De mis amigos</a><a id="categoria" class="menu" href="#">Por categoría</a></div>
+        <div class="tabs"><a id="mismemes" href="#" class="selectedTab menu">Mis Memes</a><a  id="misamigos"  class="menu" href="#">De mis amigos</a><a id="categoria" class="menu" href="#">Por categoría</a></div>
         <div class="memeThumbs">
           <div class="itemMeme"><a href="<?php echo CController::CreateUrl('Comics/create'); ?>" class="itemAction"><i class="icon-plus-sign"></i>Crea Nuevo Meme</a></div>
               <? 
