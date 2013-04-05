707ce1dc7651aaccacd44d4630fa9cb0009c97a1
Oscar <oscarolbe@hotmail.com>
2013-04-04 22:10:51 -0600

    create

--- protected/views/comics/create.php
+++ protected/views/comics/create.php
@@ -46,7 +46,9 @@
                 ?>    
           </div>
           <div id="tab3" class="memeThumbs">
-            <? /*
+            <? 
+                echo '<div class="itemMeme">'.CHtml::image(Yii::app()->request->baseUrl."/images/placeholder.png","caras",array('id'=>1,'data-original'=>Yii::app()->request->baseUrl."/Comics/comic1.jpg",'class'=>'lazy')).'</div>'; 
+            /*
                 $bandera=false;
                     echo '<ul class="js-slides-1 bx-slides">';
                     foreach ($backgrounds as $key => $value) {  
@@ -71,7 +73,7 @@
 <?php echo $this->renderPartial('_form', array('model'=>$model)); 
 
 Yii::app()->getClientScript()->registerScript('registrar', '
-  var angle, cara, cara_web, cuerpo, ojos, boca, currentLayer, currentSelected, imageCabeza, imageCuerpo, imageOjos, imageBoca, init, insertCabeza, insertCuerpo, layerPersonaje, listenerStat, newangle, rotateLeft, rotateRight, saveToImage, sendBack, sendFront, stagePersonaje;
+  var angle,cuerpos, amigos, currentLayer, currentSelected, imageCabeza, imageCuerpo, imageOjos, imageBoca, init, insertCabeza, insertCuerpo, layerPersonaje, listenerStat, newangle, rotateLeft, rotateRight, saveToImage, sendBack, sendFront, stagePersonaje;
   
   currentSelected = null;
 
@@ -80,19 +82,19 @@ Yii::app()->getClientScript()->registerScript('registrar', '
     width: 640,
     height: 480,
   });
-
+  
+  layerFondo = new Kinetic.Layer();
   layerPersonaje = new Kinetic.Layer();
+  stagePersonaje.add(layerFondo);
   stagePersonaje.add(layerPersonaje);
 
 //BOTONES
 
   init = function() {
     console.log("ok go");
-    $("#tab1 div").on("click", function(e){ var pieza = $(this).find("img").attr("id").split("-"); insertarPieza("cara",100,100,0,pieza[0],pieza[1],$(this).find("img").attr("src")) });
-    $("#tab2 div").on("click", function(e){ var pieza = $(this).find("img").attr("id").split("-"); insertarPieza("cuerpo",100,100,0,pieza[0],pieza[1],$(this).find("img").attr("src")) });
-    $("#tab3 div").on("click", function(e){ var pieza = $(this).find("img").attr("id").split("-"); insertarAccesorio(100,100,0,pieza[0],pieza[1],$(this).find("img").attr("src")) });
-    $("#tab4 div").on("click", function(e){ var pieza = $(this).find("img").attr("id").split("-"); insertarPieza("ojos",100,100,0,pieza[0],pieza[1],$(this).find("img").attr("src")) });
-    $("#tab5 div").on("click", function(e){ var pieza = $(this).find("img").attr("id").split("-"); insertarPieza("boca",100,100,0,pieza[0],pieza[1],$(this).find("img").attr("src")) });
+    $("#tab1 div").on("click", function(e){ var pieza = $(this).find("img").attr("id"); insertar("amigo",100,100,0,pieza[0],pieza[1],$(this).find("img").attr("src")) });
+    $("#tab2 div").on("click", function(e){ var pieza = $(this).find("img").attr("id"); insertar("cuerpo",100,100,0,pieza[0],pieza[1],$(this).find("img").attr("src")) });
+    $("#tab3 div").on("click", function(e){ var pieza = $(this).find("img").attr("id"); insertarFondo(100,100,0,pieza[0],pieza[1],$(this).find("img").attr("src")) });
     $("#js-toImage").on("click", saveToImage);
     $("#js-listenerStat").on("click", listenerStat);
     $("#js-rotateLeft").on("click", rotateLeft);
@@ -102,53 +104,79 @@ Yii::app()->getClientScript()->registerScript('registrar', '
     return $("#js-sendBack").on("click", sendBack);
   };
 
-  function insertarPieza(obj,x,y,rotation,pieza_id,tipo_pieza_id,img) {
+  function insertar(obj,pieza_id,img) {
+  	var aux;
+  	if(obj==="cuerpo"){ aux=obj; obj=cuerpos; }
+    if(obj==="amigo"){ aux=obj; obj=amigos; }
+    var insertar=true;
+    for(i=0;i<accesorios.length;i++){
+      if(accesorios[i].attrs.id == pieza_id) insertar=false;
+    }
+    if(insertar){
+      imageAccesorio = new Image();
+      accesorio = new Kinetic.Image({
+        x: x,
+        y: y,
+        rotation: rotation,
+        height: 200,
+        width: 200,
+        image: imageAccesorio,
+        draggable: true,
+        offset: [100, 100],
+        tipo: tipo_pieza_id,
+        id: pieza_id
+      });
+      console.log(img);
+      img=img.replace(/^.*\/(?=[^\/]*$)/, "");
+      console.log(img);
+      imageAccesorio.src="'.Yii::app()->request->baseUrl.'/img/"+img;
+      accesorio.on("mouseover", function() {
+        this.setStroke("980d2e");
+        this.setStrokeWidth(1);
+        return layerPersonaje.draw();
+      });
+
+      accesorio.on("mouseout", function() {
+        this.setStroke(null);
+        this.setStrokeWidth(0);
+        return layerPersonaje.draw();
+      });
+
+      accesorio.on("click", function() {
+        currentSelected = this;
+        return currentLayer = layerPersonaje;
+      });
+      console.log("ACCESORIO: id: "+pieza_id+"tipo: "+tipo_pieza_id);
+      layerPersonaje.add(accesorio);
+      accesorios.push(accesorio);
+      layerPersonaje.draw();
+      return true;
+    }
     
-    var image = new Image();
-    obj = new Kinetic.Image({
-      x: x,
-      y: y,
-      rotation: rotation,
-      height: 200,
-      width: 200,
-      image: image,
-      draggable: true,
-      offset: [100, 100],
-      tipo: tipo_pieza_id,
-      id: pieza_id
-    });
-    layerPersonaje.add(obj);
-    img=img.replace(/^.*\/(?=[^\/]*$)/, "");
-    console.log(img);
-    
-    obj.on("mouseover", function() {
-      this.setStroke("980d2e");
-      this.setStrokeWidth(1);
-      return layerPersonaje.draw();
-    });
-
-    obj.on("mouseout", function() {
-      this.setStroke(null);
-      this.setStrokeWidth(0);
-      return layerPersonaje.draw();
-    });
-
-    obj.on("click", function() {
-      currentSelected = this;
-      return currentLayer = layerPersonaje;
-    });
-
-    layerPersonaje.draw();
+    console.log("NO SE INSERTO");
+    return false;
   };
 
-  saveToImage = function() {
-    stagePersonaje.toDataURL({
-      callback: function(dataUrl) {
-        alert(dataUrl);
-        //return window.open(dataUrl);
-      }
-    });
-    return false;
+  function insertarFondo(obj,img) {
+  	
+  	imageFondo = new Image();
+    fondo = new Kinetic.Image({
+        x: 0,
+        y: 0,
+        rotation: rotation,
+        height: 640,
+        width: 480,
+        image: imageBackground,
+        draggable: true,
+        offset: [100, 100],
+        tipo: tipo_pieza_id,
+        id: pieza_id
+      });
+	imageBackground = new Image();
+	imageBackground.src="'.Yii::app()->request->baseUrl.'/Comics/comic1.jpg";
+	layerFondo.add(fondo);
+	layerPersonaje.moveToTop();
+
   };
 
   listenerStat = function() {
@@ -181,11 +209,14 @@ Yii::app()->getClientScript()->registerScript('registrar', '
   removeImage = function(){
     //layerPersonaje.remove();
     for(i=0;i<accesorios.length;i++){
-      if(accesorios[i].attrs.id == currentSelected.attrs.id) accesorios.setVisible(false);
+      if(accesorios[i].attrs.id == currentSelected.attrs.id){
+        o = accesorios.indexOf(currentSelected)
+        delete accesorios[o];
+        accesorios.splice(o,o+1);
+      }
         //accesorios.remove(currentSelected);
     }
     currentSelected.remove();
-    //currentSelected.setVisible(false);
     currentLayer.draw();
   }
 
