3c898b685b37136ce7d443b23f6a7e89e7d88fe9
Oscar <oscarolbe@hotmail.com>
2013-04-08 11:34:17 -0500

    redirect

--- protected/controllers/ComicsController.php
+++ protected/controllers/ComicsController.php
@@ -91,7 +91,7 @@ class ComicsController extends Controller
                  $modelRelComics->tbl_comics_id=$model->id;
                  if($modelRelComics->save()){
                     $this->ShareComic($my_access_token,'https://apps.t2omedia.com.mx/php2/jcuervo/Comics/'.$filename,'Meme');
-	       	 		$this->redirect(array('App/profile','id'=>$model->Usuario->id_facebook));
+	       	 		$this->redirect(array('App/profile','id'=>$model->UsuariosComics[0]->Usuario->id_facebook));
                  }
 
 			}
