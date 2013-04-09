e3e6db18df54788da8d2ec944926117d9b07e933
Jair <dev.lnx1337@gmail.com>
2013-04-09 01:32:22 -0500

    otro

--- protected/views/app/_filtros.php
+++ protected/views/app/_filtros.php
@@ -4,7 +4,7 @@
 $cantidad_resultados=count($resultado);
 for($i=0;$i<$cantidad_resultados;$i++){
 
-             echo '<div class="itemMeme"><a data-fancybox-type="iframe" href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/detalle/'.$resultado[$i]->id.'"  class="js-lightbox">'.CHtml::image(Yii::app()->request->baseUrl."/Comics/".$resultado[$i]->Comic->imagen).'</a><div><a href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/'.$resultado[$i]->Usuario->id_facebook.'">'.CHtml::image('https://graph.facebook.com/'.$resultado[$i]->Usuario->id_facebook.'/picture').'</a></div>
+             echo '<div class="itemMeme"><a data-fancybox-type="iframe" href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/detalle/'.$resultado[$i]->Comic->id.'"  class="js-lightbox">'.CHtml::image(Yii::app()->request->baseUrl."/Comics/".$resultado[$i]->Comic->imagen).'</a><div><a href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/'.$resultado[$i]->Usuario->id_facebook.'">'.CHtml::image('https://graph.facebook.com/'.$resultado[$i]->Usuario->id_facebook.'/picture').'</a></div>
                  </div>';
 }
 ?>
