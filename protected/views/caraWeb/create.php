987ce317ba3a00d8dea26a170681b431286e817a
Oscar <oscarolbe@hotmail.com>
2013-04-08 19:15:38 -0500

    a

--- protected/views/caraWeb/create.php
+++ protected/views/caraWeb/create.php
@@ -10,12 +10,34 @@
   <body class="lb">
    
      
-<h1>Create CaraWeb</h1>
+	<h1>Tomar  Fotografia</h1>
 
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+	<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
 
-   
-    
+
+<?php  
+  Yii::app()->clientScript->registerCoreScript('jquery');
+  $baseUrl = Yii::app()->baseUrl; 
+  $cs = Yii::app()->getClientScript();
+  $cs->registerScriptFile($baseUrl.'/js/webcam.js');
+  
+  $cs->registerScript(
+  'configuration-script_webcam',
+  'webcam.set_api_url("/noosbit_pf/index.php/foto/SaveFoto");
+		webcam.set_quality( 90 ); // JPEG quality (1 -100)
+		webcam.set_shutter_sound( true ); // play shutter click sound',
+  CClientScript::POS_HEAD
+);
+
+  $cs->registerScript(
+  'jquery',
+  ' var  visible=0;
+		$(".espacio_camara").before(webcam.get_html(320, 250));
+	;       ',
+  CClientScript::POS_READY
+);
+?>
+
 
      
     <script type="text/javascript" src="/php2/jcuervo/assets/11f59b72/jquery.js"></script>
