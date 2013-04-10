c2cfd1b6ab0eb8f32d2df344989518ab9bd4829f
Jair <dev.lnx1337@gmail.com>
2013-04-10 18:16:52 -0500

    otro

--- protected/views/caraWeb/create.php
+++ protected/views/caraWeb/create.php
@@ -15,14 +15,13 @@
   <body class="lb">
 
    <script type="text/javascript">
-  $(function(){
 
     $('#cropbox').Jcrop({
       aspectRatio: 1,
       onSelect: updateCoords
     });
 
-  });
+  
   function updateCoords(c)
   {
     $('#x').val(c.x);
