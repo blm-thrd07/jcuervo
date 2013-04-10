721198150cb69dc7c5199f7321c8963b1e5015cf
Jair <dev.lnx1337@gmail.com>
2013-04-10 17:10:09 -0500

    cambio camara

--- protected/views/caraWeb/create.php
+++ protected/views/caraWeb/create.php
@@ -32,9 +32,6 @@
   
 <script type="text/javascript" src="/php2/jcuervo/assets/11f59b72/jquery.js"></script>
 <script language="JavaScript">
- 
-/////termina jcrops
-
   var  visible=0; 
   $(".espacio_camara").before(webcam.get_html(320, 250));
   //webcam.set_api_url("/jcuervo/index.php/CaraWeb/SaveFoto");
@@ -56,45 +53,13 @@
       webcam.reset();
     }
     else { 
-               alert("error");
-  //document.getElementById('upload_results').innerHTML = '<img src="' + msg + '" width="300" heigth="300" >';
-      
+          alert("error");
     };
   }
 </script>
 
 
-
-<?php  
-  $baseUrl = Yii::app()->baseUrl; 
-  $cs = Yii::app()->getClientScript();
-  //$cs->registerScriptFile($baseUrl.'/js/webcam.js');
-  $cs->registerScript(
-  'configuration-script_webcam',' alert("conf");
-  		webcam.set_api_url("/jcuervo/index.php/CaraWeb/SaveFoto");
-		webcam.set_quality( 90 ); // JPEG quality (1 -100)
-		webcam.set_shutter_sound( true ); // play shutter click sound',
-  CClientScript::POS_END
-);
-
-  $cs->registerScript(
-  'jquery',' 
-  		var  visible=0; alert("before");
-		$(".espacio_camara").before(webcam.get_html(320, 250));
-	',
-  CClientScript::POS_READY
-);
-?>
-
-
-     
     <script type="text/javascript" src="/php2/jcuervo/assets/11f59b72/jquery.js"></script>
-    <!-- <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/kinetic-v4.3.3.min.js"></script>
-    <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.lazyload.min.js"></script>
-	<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.easytabs.min.js"></script>
-    <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.hashchange.min.js"></script>
-    <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.bxslider.min.js"></script>
-    <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/scripts.js"></script>-->
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/fancybox/jquery.fancybox.pack.js"></script>
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/all.js"></script>
 
