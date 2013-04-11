e1a0c0183ef348ed5fa4b156c6526b4b267e4329
Jair <dev.lnx1337@gmail.com>
2013-04-11 12:46:14 -0500

    otro

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -6,10 +6,12 @@
     <title>Memegenerator Jose Cuervo Especial</title>
     <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/styles.css">
     <?  $idFb = split('/', $_SERVER['PATH_INFO']); if(count($idFb)==4){ if($idFb[2]=='Profile'){ Yii::app()->session['nidFb']=$idFb[3]; } } 
-        echo Yii::app()->session['nidFb'];
         $protocol="http://"; if(isset($_SERVER['HTTPS'])){ $protocol="https://"; }else{ $protocol="http://"; }
     ?>
-    <script> var iU=<? echo Yii::app()->session['nidFb']; ?>; var protocol=<? echo $protocol; ?>; </script> 
+    <script> 
+             var iU=<? echo Yii::app()->session['nidFb']; ?>; 
+             var protocol=<? echo $protocol; ?>; 
+    </script> 
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/all.js"></script>
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/fancybox/jquery.fancybox.pack.js"></script>
     <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/fancybox/jquery.fancybox.css">
