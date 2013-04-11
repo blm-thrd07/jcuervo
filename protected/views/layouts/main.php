6c1c5f1ea78b2bb94ce6d0e6c5ac8b45398bda27
Jair <dev.lnx1337@gmail.com>
2013-04-11 12:35:29 -0500

    otro

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -5,7 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Memegenerator Jose Cuervo Especial</title>
     <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/styles.css">
-    <?  $idFb = split('/', $_SERVER['PATH_INFO']); if(count($idFb)==4){ if($idFb[2]=='Profile'){ Yii::app()->session['nidFb']=$idFb[3]; } } ?>
+    <?  $idFb = split('/', $_SERVER['PATH_INFO']); if(count($idFb)==4){ if($idFb[2]=='Profile'){ Yii::app()->session['nidFb']=$idFb[3]; } } 
+
+if(isset($_SERVER['HTTPS'])){
+
+  echo "en https";
+}else{
+
+  echo "en http";
+}
+
+    ?>
     <script> var iU=<? echo Yii::app()->session['nidFb']; ?>; </script> 
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/all.js"></script>
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/fancybox/jquery.fancybox.pack.js"></script>
