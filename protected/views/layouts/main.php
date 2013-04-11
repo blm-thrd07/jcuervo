4ae7ac91a48a3dd2fec50977d19d6a46bcc26735
Jair <dev.lnx1337@gmail.com>
2013-04-11 12:44:02 -0500

    otro

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -6,7 +6,8 @@
     <title>Memegenerator Jose Cuervo Especial</title>
     <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/styles.css">
     <?  $idFb = split('/', $_SERVER['PATH_INFO']); if(count($idFb)==4){ if($idFb[2]=='Profile'){ Yii::app()->session['nidFb']=$idFb[3]; } } 
-         $protocol="http://"; if(isset($_SERVER['HTTPS'])){ $protocol="https://"; }else{ $protocol="http://"; }
+        echo $idFb;
+        $protocol="http://"; if(isset($_SERVER['HTTPS'])){ $protocol="https://"; }else{ $protocol="http://"; }
     ?>
     <script> var iU=<? echo Yii::app()->session['nidFb']; ?>; var protocol=<? echo $protocol; ?>; </script> 
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/all.js"></script>
