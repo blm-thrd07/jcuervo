c0a6ae3f4d6ff7db04769124d2a9d787036f2070
Gabo Lugo <glsmaster.xg@gmail.com>
2013-04-15 16:38:44 -0500

    comentarios

--- protected/views/app/_detalle.php
+++ protected/views/app/_detalle.php
@@ -15,14 +15,11 @@
   </head>
 
   <body class="lb">
+
     <div id="detalle">
-      <div id="pic">
-        <? echo CHtml::image(Yii::app()->request->baseUrl."/Comics/".$json['comic']['comic']['imagen']); ?>
-          <? if($json['comic']['comic']['eliminar']){ ?>
-             <a href="#" class="btn delc" id="<? echo $json['comic']['comic']['id']; ?>"><i class="icon-trash"></i> Eliminar</a>
-         <? } ?>     
-        <div><span id="NoCompartido"><? echo $json['comic']['comic']['NoCompartido']; ?></span><a href="#" id="<? echo $json['comic']['comic']['id'];  ?>" class="btn share"><i class="icon-share"></i> Compartir</a></div>
-      </div>
+
+      
+
       <div id="comentarios">
         <div><?echo CHtml::image('https://graph.facebook.com/'.$json['comic']['usuario']['idFb'].'/picture')?><span><? echo $json['comic']['usuario']['nombre']; ?></span></div>
         <form>
@@ -33,32 +30,42 @@
         <div id="comics">
        
 
-       <? 
-           if(is_array($json['comic']['comic']['comments'])){
-                foreach ($json['comic']['comic']['comments'] as $key => $value) {
-        ?>        
-         <article id="comentario<? echo $value['id']; ?>">
-          <h3><?echo $value['nombre']; ?></h3>
-          <p><? echo $value['comment'] ?></p>
-          <? if($value['delete']){ ?>
-             <a href="#" class="btn delcom" id="<? echo $value['id']; ?>"><i class="icon-trash"></i> Eliminar</a>
-          <? } ?>
-        </article>
-              <?        
-                }
-              }
-            ?>
+          <?
+            if(is_array($json['comic']['comic']['comments'])){
+              foreach ($json['comic']['comic']['comments'] as $key => $value) {
+          ?>
+          
+          <article id="comentario<? echo $value['id']; ?>">
+            <h3><?echo $value['nombre']; ?></h3>
+            <p><? echo $value['comment'] ?></p>
+            <? if($value['delete']){ ?>
+               <a href="#" class="btn delcom" id="<? echo $value['id']; ?>"><i class="icon-trash"></i> Eliminar</a>
+            <? } ?>
+          </article>
+          
+          <?
+            }
+          }
+          ?>
       
         </div>
       </div>
+
+
+      <div id="pic">
+        <? echo CHtml::image(Yii::app()->request->baseUrl."/Comics/".$json['comic']['comic']['imagen']); ?>
+          <? if($json['comic']['comic']['eliminar']){ ?>
+             <a href="#" class="btn delc" id="<? echo $json['comic']['comic']['id']; ?>"><i class="icon-trash"></i> Eliminar</a>
+         <? } ?>     
+        <div><span id="NoCompartido"><? echo $json['comic']['comic']['NoCompartido']; ?></span><a href="#" id="<? echo $json['comic']['comic']['id'];  ?>" class="btn share"><i class="icon-share"></i> Compartir</a></div>
+      </div>
+
     </div>
       
-    <script type="text/javascript" src="/php2/jcuervo/assets/11f59b72/jquery.js"></script>
+    <script src="/php2/jcuervo/assets/11f59b72/jquery.js"></script>
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/kinetic-v4.3.3.min.js"></script>
-    <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.lazyload.min.js"></script>
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.easytabs.min.js"></script>
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.hashchange.min.js"></script>
-    <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.bxslider.min.js"></script>
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/fancybox/jquery.fancybox.pack.js"></script>
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/all.js"></script>
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/scripts.js"></script>
