e9baca1864708954f33ad8e701c510692d707414
Gabo Lugo <glsmaster.xg@gmail.com>
2013-04-15 01:45:06 -0500

    no minificamos el css, no comentarios de compass

--- config.rb
+++ config.rb
@@ -1,20 +1,14 @@
-# Require any additional compass plugins here.
-
-# Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
 
-# You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :compressed
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
+output_style = :expanded
+preferred_syntax = :sass
+environment = :production
 relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
-
+color_output = false
 preferred_syntax = :sass
