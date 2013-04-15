9db25793edfc593ef09204e0d532c1645e428aed
Jair <dev.lnx1337@gmail.com>
2013-04-14 23:51:47 -0500

    cambio

--- protected/views/app/profile.php
+++ protected/views/app/profile.php
@@ -1,7 +1,9 @@
  <section id="panelPersonaje">
         <h1><?echo $json['usuario']['nombre']; ?></h1>
         <div><? echo "<img src='https://apps.t2omedia.com.mx/php2/jcuervo/Avatar/".$json['usuario']['avatar_img']."' />"; ?></div>
+        <? if(Yii::app()->session['id_facebook']==$json['usuario']['id_facebook']){ ?>
         <div id="actions"><a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/avatars/update/<?echo Yii::app()->session['usuario_id']; ?>" class="btn"><i class="icon-edit"></i> Editar</a></div>
+        <? }?>
 </section>
 <section id="panelContent">
      
