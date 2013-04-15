cf4c950df5d6ed96ca6297b42d844440d759df82
Gabo Lugo <glsmaster.xg@gmail.com>
2013-04-15 16:46:45 -0500

    delete icon

--- protected/views/app/_detalle.php
+++ protected/views/app/_detalle.php
@@ -39,7 +39,7 @@
             <h3><?echo $value['nombre']; ?></h3>
             <p><? echo $value['comment'] ?></p>
             <? if($value['delete']){ ?>
-               <a href="#" class="btn delcom" id="<? echo $value['id']; ?>"><i class="icon-trash"></i> Eliminar</a>
+               <a href="#" class="btn delcom" id="<? echo $value['id']; ?>"><i class="icon-trash"></i></a>
             <? } ?>
           </article>
           
