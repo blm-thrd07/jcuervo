a2c69a6590c6951ca836db8635826654b4cbd9a4
Oscar <oscarolbe@hotmail.com>
2013-04-16 18:16:52 -0500

    as

--- protected/views/caraWeb/create.php
+++ protected/views/caraWeb/create.php
@@ -1,31 +1,23 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="https://www.facebook.com/2008/fbml">
+<html lang="es">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Memegenerator Jose Cuervo Especial</title>
-
     <style type="text/css"> .espacio_camara{ background-color: orange; height: auto; }</style>
     <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/styles.css">
-    <link rel="stylesheet" href="/php2/jcuervo/css/jquery.Jcrop.css" type="text/css" />
-    
-    <script src="/php2/jcuervo/assets/11f59b72/jquery.js"></script>
-    <script src="/php2/jcuervo/js/jquery.Jcrop.js"></script>
+    <script type="text/javascript" src="/php2/jcuervo/assets/11f59b72/jquery.js"></script>
+    <script type="text/javascript" src="/php2/jcuervo/js/jquery.Jcrop.js"></script>
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/webcam.js"></script>
-    
-   
-
+    <link rel="stylesheet" href="/php2/jcuervo/css/jquery.Jcrop.css" type="text/css" />
     <?  $idFb = split('/', $_SERVER['PATH_INFO']); if(count($idFb)==4){ if($idFb[2]=='Profile'){ Yii::app()->session['nidFb']=$idFb[3]; } } 
         $protocol="http://"; if(isset($_SERVER['HTTPS'])){ $protocol="https://"; }else{ $protocol="http://"; }
     ?>
     <script>  window.protocol="<? echo $protocol; ?>"; </script> 
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/scripts.js"></script>
   </head>
-
-
-
   <body class="lb">
-  <div id="fb-root"></div>
+  
   <div id="upload_results"></div>
   <div id="camaradiv"> 
     <h1>Tomar  Fotografia</h1>
@@ -35,8 +27,8 @@
         <input class="btn" type="button" value="Tomar foto" onClick="webcam.freeze()" > 
         <input class="btn" type="button" value="Guardar" onClick="do_upload()" > 
         <input class="btn" type="button" value="Otra vez" onClick="webcam.reset()" > 
-    </div>
-  </div>
+      </div>
+   </div>
   
   <script language="JavaScript">
   var  visible=0; 
