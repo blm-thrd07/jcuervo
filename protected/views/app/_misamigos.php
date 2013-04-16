5137a53575e8f389ebca406289bdc7cb94079cc8
Jair <dev.lnx1337@gmail.com>
2013-04-16 18:06:25 -0500

    mis amigos

--- protected/views/app/_misamigos.php
+++ protected/views/app/_misamigos.php
@@ -5,8 +5,6 @@
 
        <div class="js-slides">
           <div class="slides_container">
-
-
             
           <? 
           if(((int)$cantidad_amigos)==0){?>
@@ -25,8 +23,7 @@
 
            if(count($comicsAmigos)!=0){
             foreach ($comicsAmigos as $key => $value) { 
-
-        echo '<div class="itemThumbnail"><a data-fancybox-type="iframe" href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/detalle/'.$value["id"].'"  id="'.$value["id"].'"  class="js-lightbox cdetail">'.CHtml::image(Yii::app()->request->baseUrl."/Comics/".$value['imagen']).'</a><div><a href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/'.$value['id_facebook'].'">'.CHtml::image('https://graph.facebook.com/'.$value['id_facebook'].'/picture').'</a></div></div></div>';
+                    echo '<div class="itemThumbnail"><a data-fancybox-type="iframe" href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/detalle/'.$value["id"].'"  id="'.$value["id"].'"  class="js-lightbox cdetail">'.CHtml::image(Yii::app()->request->baseUrl."/Comics/".$value['imagen']).'</a><div><a href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/'.$value['id_facebook'].'">'.CHtml::image('https://graph.facebook.com/'.$value['id_facebook'].'/picture').'</a></div></div>';
               }
              
             }  
@@ -34,9 +31,6 @@
           ?>
 
         </div>
-
-
-
 
 
 
