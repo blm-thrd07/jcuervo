c5e7d6eaf4e03c5ec2b316f8e21d4e2444505c08
Oscar <oscarolbe@hotmail.com>
2013-04-16 18:51:27 -0500

    s

--- protected/views/caraWeb/create.php
+++ protected/views/caraWeb/create.php
@@ -13,7 +13,6 @@
     <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/fancybox/jquery.fancybox.css">
     
     <script src="/php2/jcuervo/js/jquery.Jcrop.js"></script>
-    <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/webcam.js"></script>
     <link rel="stylesheet" href="/php2/jcuervo/css/jquery.Jcrop.css" type="text/css" />
     <?  $idFb = split('/', $_SERVER['PATH_INFO']); if(count($idFb)==4){ if($idFb[2]=='Profile'){ Yii::app()->session['nidFb']=$idFb[3]; } } 
         $protocol="http://"; if(isset($_SERVER['HTTPS'])){ $protocol="https://"; }else{ $protocol="http://"; }
@@ -36,32 +35,7 @@
    </div>
   
   <script language="JavaScript">
-  var  visible=0; 
-  $(".espacio_camara").before(webcam.get_html(310, 250));
-  //webcam.set_api_url("/jcuervo/index.php/CaraWeb/SaveFoto");
-  //webcam.set_quality( 90 ); // JPEG quality (1 -100)
-  //webcam.set_shutter_sound( true ); // play shutter click sound
-  webcam.set_hook( 'onComplete', 'my_completion_handler' );
-  function do_upload() {
-    // upload to server
-    document.getElementById('upload_results').innerHTML = '<h1>Guardando foto...</h1>';
-    webcam.upload();
-  }
-  function my_completion_handler(msg) {
-    // extract URL out of PHP output
-    if (msg.match(/(http\:\/\/\S+)/)) {
-      var image_url = RegExp.$1;
-      //webcam.reset();
-
-      if(image_url!=null){
-        $('#camaradiv').css('display','none');
-        document.getElementById('upload_results').innerHTML = '<h1>Delimita tu rostro dando click!</h1><br><img src="'+image_url+'" id="cropbox">'+'<button id="spic" class="btn" >Save!!</button>';       
-       }
-    }
-    else { 
-          alert("error");
-    };
-  }
+ 
 </script>
  
  
