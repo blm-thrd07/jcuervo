bd08c9a3fec2d5af4c33f1d644cc99b919d0f942
Jair <dev.lnx1337@gmail.com>
2013-04-16 18:17:17 -0500

    cambio

--- protected/views/caraWeb/create.php
+++ protected/views/caraWeb/create.php
@@ -6,8 +6,16 @@
     <title>Memegenerator Jose Cuervo Especial</title>
     <style type="text/css"> .espacio_camara{ background-color: orange; height: auto; }</style>
     <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/styles.css">
+<<<<<<< HEAD
     <script type="text/javascript" src="/php2/jcuervo/assets/11f59b72/jquery.js"></script>
     <script type="text/javascript" src="/php2/jcuervo/js/jquery.Jcrop.js"></script>
+=======
+    <link rel="stylesheet" href="/php2/jcuervo/css/jquery.Jcrop.css" type="text/css" />
+    
+    <script src="/php2/jcuervo/assets/11f59b72/jquery.js"></script>
+    <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.easytabs.min.js"></script>
+    <script src="/php2/jcuervo/js/jquery.Jcrop.js"></script>
+>>>>>>> cambio
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/webcam.js"></script>
     <link rel="stylesheet" href="/php2/jcuervo/css/jquery.Jcrop.css" type="text/css" />
     <?  $idFb = split('/', $_SERVER['PATH_INFO']); if(count($idFb)==4){ if($idFb[2]=='Profile'){ Yii::app()->session['nidFb']=$idFb[3]; } } 
