695cf52ef2ae1f0b135d73217ccd8f8c002b7781
Oscar <oscarolbe@hotmail.com>
2013-04-16 19:05:02 -0500

    s

--- js/webcam.js
+++ js/webcam.js
@@ -24,7 +24,7 @@ window.webcam = {
 	ie: !!navigator.userAgent.match(/MSIE/),
 	//protocol: location.protocol.match(/https/i) ? 'https' : 'http',
 	callback: null, // user callback for completed uploads
-	swf_url: location.protocol.match(/https/i) ? 'https' : 'http' + '://apps.t2omedia.com.mx/php2/jcuervo/js/webcam.swf', // URI to webcam.swf movie (defaults to cwd)
+	swf_url: 'https://apps.t2omedia.com.mx/php2/jcuervo/js/webcam.swf', // URI to webcam.swf movie (defaults to cwd)
 	shutter_url: location.protocol.match(/https/i) ? 'https' : 'http' + '://apps.t2omedia.com.mx/php2/jcuervo/js/shutter.mp3', // URI to shutter.mp3 sound
 	api_url: location.protocol.match(/https/i) ? 'https' : 'http' + '://apps.t2omedia.com.mx/php2/jcuervo/index.php/CaraWeb/SaveFoto', // URL to upload script
 	loaded: false, // true when webcam movie finishes loading
