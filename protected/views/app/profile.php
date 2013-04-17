8c6cf2eb2740823988246bb00b21a21ce737a221
Jair <dev.lnx1337@gmail.com>
2013-04-16 19:38:22 -0500

    menu changeu

--- protected/views/app/profile.php
+++ protected/views/app/profile.php
@@ -11,10 +11,21 @@
      <? if(Yii::app()->session['id_facebook']==$json['usuario']['id_facebook']){ ?>
         <h2>Mis Memes</h2><a href="<?php echo CController::CreateUrl('Comics/create'); ?>">Crea un meme nuevo</a>
      <? } else {?>
-         <h2>Memes de <? echo $json['usuario']['nombre'] ?></h2>
+         <h2>Memes de <? echo $json['usuario']['nombre'] ?></h2><a href="<?php echo CController::CreateUrl('Comics/create'); ?>">Crea un meme nuevo</a>
      <? } ?>
     
-<div class="tabs"><a  href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/<? echo Yii::app()->session['id_facebook']; ?>" class="selectedTab">Mis Memes</a><a  id="misamigos"  class="menu" href="#">De mis amigos</a><a id="categoria" class="menu" href="#">Por categoría</a></div>
+<div class="tabs">
+  <? if(Yii::app()->session['id_facebook']==$json['usuario']['id_facebook']){ ?>
+
+  <a  href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/<? echo Yii::app()->session['id_facebook']; ?>"  class="selectedTab">Mis Memes</a>
+      <? } else {?>
+  <a  href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/<? echo Yii::app()->session['id_facebook']; ?>">Mis Memes</a>
+
+
+      <? } ?>
+
+
+  <a  id="misamigos"  class="menu" href="#">De mis amigos</a><a id="categoria" class="menu" href="#">Por categoría</a></div>
 
 <div class="js-slides">
     <div class="slides_container">        
