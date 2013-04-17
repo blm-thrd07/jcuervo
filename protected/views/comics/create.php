e351f81357a05651c0b159ce7b5ed86a5e16244b
Oscar <oscarolbe@hotmail.com>
2013-04-16 20:27:48 -0500

    thumbnail

--- protected/views/comics/create.php
+++ protected/views/comics/create.php
@@ -71,9 +71,7 @@
                   if($bandera) echo '</div>'; else $bandera=true;
                   echo '<div class="slide">';
                 }
-                echo '<div class="itemMeme">'.CHtml::image(Yii::app()->request->baseUrl."/Avatar/".$value['avatar_img'],"amigos_avatars",array('id'=>$value['usuario_id'])).'<div class="itemThumbnail"><a data-fancybox-type="iframe" href="detalle.html" class="js-lightbox"><img src="http://ima.gs/transparent/000000/A2A2A2/640x480.png"></a>
-                <div><a href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/'.$value['idFb'].'">'.CHtml::image('https://graph.facebook.com/'.$value['idFb'].'/picture').'</a></div>
-              </div> </div>'; 
+                echo '<div class="itemThumbnail">'.CHtml::image(Yii::app()->request->baseUrl."/Avatar/".$value['avatar_img'],"amigos_avatars",array('id'=>$value['usuario_id'])).'<div><a href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/'.$value['idFb'].'">'.CHtml::image('https://graph.facebook.com/'.$value['idFb'].'/picture').'</a></div></div>'; 
               }
               if($count>12) echo '</div></div><a class="prev"><i class="icon-chevron-left"></i></a><a class="next"><i class="icon-chevron-right"></i></a></div>';//btns pre <a ....
             }
