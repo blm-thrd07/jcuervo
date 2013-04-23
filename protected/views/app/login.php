0c0f42a4c740b9d32625b799ec7fd23730a3c320
Jair <dev.lnx1337@gmail.com>
2013-04-23 13:20:47 -0500

    camnio

--- protected/views/app/login.php
+++ protected/views/app/login.php
@@ -27,11 +27,12 @@ header('P3P:CP="IDC DSP COR ADM DEVi TAIi PSA PSD IVAi IVDi CONi HIS OUR IND CNT
     </div>
 
 <script>
+/*
   var oauth_url = 'https://www.facebook.com/dialog/oauth/';
   oauth_url += '?client_id=342733185828640';
   oauth_url += '&redirect_uri=' + encodeURIComponent('http://www.facebook.com/Lnx1337?sk=app_342733185828640');
   oauth_url += '&scope=email,read_stream,user_likes,publish_actions,publish_stream,offline_access'
-
+*/
 
   
 document.getElementById("login").onclick = function() {
