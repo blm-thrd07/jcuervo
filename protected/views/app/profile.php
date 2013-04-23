5bf1d5c051faa5e6f9e94fa0d62db81bd025f5ef
Jair <dev.lnx1337@gmail.com>
2013-04-23 15:47:42 -0500

    cuadro

--- protected/views/app/profile.php
+++ protected/views/app/profile.php
@@ -30,7 +30,7 @@
          <? if(is_array($comics['comics'])){
          if(count($comics['comics'])!=0){
            foreach ($comics['comics'] as $key => $value) {
-                 echo '<div class="itemThumbnail"><a data-fancybox-type="iframe" href="'.Yii::app()->session['protocol'].'apps.t2omedia.com.mx/php2/jcuervo/index.php/App/detalle/'.$value["id"].'" class="js-lightbox">'.CHtml::image(Yii::app()->request->baseUrl."/Comics/".$value['imagen']).'</a></div>';        
+                 echo '<div class="itemThumbnail"><div><a data-fancybox-type="iframe" href="'.Yii::app()->session['protocol'].'apps.t2omedia.com.mx/php2/jcuervo/index.php/App/detalle/'.$value["id"].'" class="js-lightbox">'.CHtml::image(Yii::app()->request->baseUrl."/Comics/".$value['imagen']).'</a></div></div>';        
              } 
            }
          }  
