56db7210f41759df1c8ca4a6ecc8cbaf562550ac
Jair <dev.lnx1337@gmail.com>
2013-04-23 13:50:49 -0500

    otro

--- protected/controllers/AppController.php
+++ protected/controllers/AppController.php
@@ -95,8 +95,8 @@ public function actionLogin(){
             Yii::app()->session['usuario_id']=$response[0]->id;
             Yii::app()->session['id_facebook']=$response[0]->id_facebook;
             Yii::app()->session['access_token']=$facebook->getAccessToken();
-            
-            $this->redirect(array('App/Profile/'.$user_profile['id']));
+            echo "login";
+           // $this->redirect(array('App/Profile/'.$user_profile['id']));
             
          }
 
