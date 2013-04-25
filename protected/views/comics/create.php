76892735a2501b0a212886e94b860a6260935fee
Oscar <oscarolbe@hotmail.com>
2013-04-25 18:55:51 -0500

    a

--- protected/views/comics/create.php
+++ protected/views/comics/create.php
@@ -398,7 +398,7 @@ Yii::app()->getClientScript()->registerScript('registrar', '
     initialText = "Inserta Texto";
 
   createText = function() {
-    var texto = new Kinetic.Text({
+    var texto = new Kinetic.LabelRect({
       x: 0,
       y: 0,
       text: initialText,
