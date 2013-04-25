b6789db0c0b509fcc939315fe1ad97731c9af50d
Oscar <oscarolbe@hotmail.com>
2013-04-25 02:55:42 -0500

    a

--- protected/views/comics/create.php
+++ protected/views/comics/create.php
@@ -370,7 +370,6 @@ Yii::app()->getClientScript()->registerScript('registrar', '
           console.log("p.x: "+e.x+ " p.y: "+e.y); 
           console.log("p.offset.left: "+stageComic.getContainer().offsetLeft+ " p.top: "+stageComic.getContainer().offsetTop); 
           console.log("p.offset.right: "+stageComic.getContainer().Right+ " p.bottom: "+stageComic.getContainer().Bottom); 
-          .offset().left
           
           trans = this.transitionTo({
             duration: 0.5,
