db345321445834c52ec85a46c7884d253264d603
Jair <dev.lnx1337@gmail.com>
2013-04-25 01:45:51 -0500

    otroa

--- protected/views/app/_categoria.php
+++ protected/views/app/_categoria.php
@@ -3,7 +3,7 @@
 
         <div class="tabs"><a   href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/<? echo Yii::app()->session['id_facebook']; ?>" >Mis Memes</a> <a  id="misamigos"  class="menu" href="">De mis amigos</a><a id="categoria" class="selectedTab menu" href="">Por categoría</a></div>
         <div class="memeThumbs">
-          <div class="itemMeme"><a href="#" id="catmasvist" class="itemAction subcat"><i class="icon-eye-open"></i>Los + Vistos</a></div>
+          <div class="itemMeme"><a href="#" id="catmasvist" class="itemAction subcat selectedTab"><i class="icon-eye-open"></i>Los + Vistos</a></div>
           <div class="itemMeme"><a href="#" id="catmascomp" class="itemAction subcat"><i class="icon-plus-sign"></i>Los + Compartidos</a></div>
           <div class="itemMeme"><a href="#" id="catmascome" class="itemAction subcat"><i class="icon-share-alt"></i>Los + Comentados</a></div>
           <div class="itemMeme"><a href="#" id="catjoscuer" class="itemAction subcat"><i class="icon-star"></i>Seleccion Especial José Cuervo</a></div>
