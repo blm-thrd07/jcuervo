20467b8b06aac64e9fb53f4c932b8d8f28c75561
Jair <dev.lnx1337@gmail.com>
2013-04-25 19:07:00 -0500

    oto

--- protected/views/app/profile.php
+++ protected/views/app/profile.php
@@ -22,7 +22,10 @@
   <a  href="<? echo Yii::app()->session['protocol']; ?>apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/<? echo Yii::app()->session['id_facebook']; ?>">Mis Memes</a>
   <? } ?>
   <a  id="misamigos"  class="menu" href="#">De mis amigos</a><a id="categoria" class="menu" href="#">Por categoría</a></div>
-
+ <? if(Yii::app()->session['id_facebook']==$json['usuario']['id_facebook']){ ?>
+     <? } else {?>
+         <h2>Memes de <? echo $json['usuario']['nombre'] ?></h2><a href="<?php echo CController::CreateUrl('Comics/create'); ?>">Crea un meme nuevo</a>
+     <? } ?>
 <div class="js-slides">
     <div class="slides_container">        
        <div class="slide itemThumbs"> 
