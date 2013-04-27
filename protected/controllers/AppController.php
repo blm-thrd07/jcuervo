58108bec7547d8d47bc89d64830d637898b84b9a
Oscar <oscarolbe@hotmail.com>
2013-04-26 19:31:03 -0500

    a

--- protected/controllers/AppController.php
+++ protected/controllers/AppController.php
@@ -69,7 +69,6 @@ public function actionLogin(){
           echo '$_REQUEST is empty';
         }
         $this->parse_signed_request($signed_request);
-
        if($user){
 
 
@@ -346,9 +345,8 @@ public function actionLogin(){
     list($encoded_sig, $payload) = explode('.', $signed_request, 2); 
     $sig = $this->base64_url_decode($encoded_sig);
     $data = json_decode($this->base64_url_decode($payload), true);
-    print_r($data);
-    //if($data['registration']['like']) {
-      /*$m = ActividadUsuario::model()->findById(array('condition'=>'usuario_id=:uid','params'=>array(':uid'=>Yii::app()->session['usuario_id'])));
+    if($data['liked']) {
+      $m = ActividadUsuario::model()->findById(array('condition'=>'usuario_id=:uid','params'=>array(':uid'=>Yii::app()->session['usuario_id'])));
       if(count($m)==0){
         $m = new ActividadUsuario;
         $m->usuario_id= Yii::app()->session['usuario_id'];
@@ -358,8 +356,8 @@ public function actionLogin(){
       }
 
       $m->save();
-      */ echo "like";
-    //}
+      
+    }
     return $data;
   }
 
