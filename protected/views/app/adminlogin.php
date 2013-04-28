491ced65646afa02c3b74243dfd33e6e9abb11a4
Oscar <oscarolbe@hotmail.com>
2013-04-28 17:23:32 -0500

    a

--- protected/views/app/adminlogin.php
+++ protected/views/app/adminlogin.php
@@ -1,4 +1,8 @@
+
+<form  method="POST">
 <label>Usuario</label>
-<input type="tetx">
+<input type="tetx" name="admin_user">
 <label>Password</label>
-<input type="password">
+<input type="password" name="admin_password">
+<input type="submit" value="Entrar">
+</form>
