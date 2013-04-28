c714f139993d90f4be483dd0748e708003f70eeb
Oscar <oscarolbe@hotmail.com>
2013-04-28 14:46:09 -0500

    admin

--- protected/views/app/admin.php
+++ protected/views/app/admin.php
@@ -1,9 +1,13 @@
 
+<h2> Administrador </h2>
+
 <?php /*echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
 <div class="search-form" style="display:none">
 <?php $this->renderPartial('_search',array(
 	'model'=>$model,
 ));*/ 
+
+
 $model = Usuarios::model()->findAll();
 
 ?>
