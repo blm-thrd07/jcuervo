f4ffdcee7984205d90c1b2bb47549d246a9d1642
Oscar <oscarolbe@hotmail.com>
2013-04-28 14:41:07 -0500

    admin usuarios

--- protected/views/app/admin.php
+++ protected/views/app/admin.php
@@ -1,3 +1,21 @@
-<?php 
-echo "admin";
+
+<?php /*echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+<div class="search-form" style="display:none">
+<?php $this->renderPartial('_search',array(
+	'model'=>$model,
+));*/ 
+$model = Usuarios::model()->findAll();
+
 ?>
+<?php $this->widget('zii.widgets.grid.CGridView', array(
+	'id'=>'admin-usuarios-grid',
+	'dataProvider'=>$model->search(),
+	'filter'=>$model,
+	/*'columns'=>array(
+		'avatar_id',
+		'url',
+		array(
+			'class'=>'CButtonColumn',
+		),
+	),*/
+)); ?>
