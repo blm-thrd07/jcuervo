9f417ffa1d2c33feeb9e75d81950de505559a8ff
Oscar <oscarolbe@hotmail.com>
2013-04-29 15:30:54 -0500

    a

--- protected/views/app/_mismemes.php
+++ protected/views/app/_mismemes.php
@@ -32,7 +32,7 @@
 
           if($count != 0){
             foreach ($comics as $key => $value) {
-              echo '<div class="itemThumbnail"><div><a data-fancybox-type="iframe" href="'.Yii::app()->session['protocol'].'apps.t2omedia.com.mx/php2/jcuervo/index.php/App/detalle/'.$value["id"].'" class="js-lightbox">'.CHtml::image(Yii::app()->request->baseUrl."/Comics/".$value['imagen']).'</a></div></div>';        
+              echo '<div class="itemMeme"><div><a data-fancybox-type="iframe" href="'.Yii::app()->session['protocol'].'apps.t2omedia.com.mx/php2/jcuervo/index.php/App/detalle/'.$value["id"].'" class="js-lightbox">'.CHtml::image(Yii::app()->request->baseUrl."/Comics/".$value['imagen']).'</a></div></div>';        
             } 
           }
           if($count>9) echo '</div></div><a class="prev"><i class="icon-chevron-left"></i></a><a class="next"><i class="icon-chevron-right"></i></a></div>';//btns pre <a ....
