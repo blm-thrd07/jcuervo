69fe1d64f9a60e527670c1283707d52bcde63bfc
Oscar <oscarolbe@hotmail.com>
2013-04-28 19:24:54 -0500

    count comics

--- protected/views/app/admin.php
+++ protected/views/app/admin.php
@@ -72,7 +72,7 @@ $this->widget('zii.widgets.grid.CGridView', array(
         array(
             'header' => 'comics creados',
 	        //'name'=>'comics',
-	        'value'=>' print_r( UsuariosHasTblComics::getMyComics($data->id) ) ',
+	        'value'=>' count( UsuariosHasTblComics::getMyComics($data->id)["comics"] ) ',
 	        'type'=>'raw',
 	        'htmlOptions'=>array('width'=>5),
 	        //'visible'=>false,
