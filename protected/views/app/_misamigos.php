50953406c385de6915ad2581e833c6b040bd45ee
Jair <dev.lnx1337@gmail.com>
2013-04-30 16:32:13 -0500

    cambio

--- protected/views/app/_misamigos.php
+++ protected/views/app/_misamigos.php
@@ -52,13 +52,14 @@
 
                   <? 
                  if($count==3){ 
-
+                     $count=1;
                     ?>
                   </div>
                  
                  <?  
+                   }else{
+                        $count++;
                    } 
-                   $count++;
                  ?>
 
                 
