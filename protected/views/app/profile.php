ee77dc47563998ac11faab693932383d05ba8002
Jair <dev.lnx1337@gmail.com>
2013-04-30 14:59:58 -0500

    prueba

--- protected/views/app/profile.php
+++ protected/views/app/profile.php
@@ -3,15 +3,6 @@
         <div class="tabs"><a  href="<? echo Yii::app()->session['protocol']; ?>apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/<? echo Yii::app()->session['id_facebook']; ?>" class="mismemesmenu" >Mis Memes</a><a  id="misamigos"  class="selectedTab menu" href="">De mis amigos</a><a id="categoria" class="menu" href="">Por categoría</a></div>
         
 
-   <? 
-          if(((int)$cantidad_amigos)==0){?>
-              <article id="noFriends">
-                <div id='fb-root'></div>
-                <h3>:(</h3>
-                <p>No tienes amigos compartiendo memes aún. Pero puedes invitar a que se unan.</p>
-                <a href="#" class="btn" onclick="FacebookInviteFriends();" ><i class="icon-group"></i> Invitar Amigos</a>
-              </article>
-          <? } ?>
      <?     
 
 /*
