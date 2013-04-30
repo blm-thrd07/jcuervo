11e99236d4ae6266b084f225a905e23d856579c9
Jair <dev.lnx1337@gmail.com>
2013-04-30 15:02:09 -0500

    mis memes

--- protected/views/app/profile.php
+++ protected/views/app/profile.php
@@ -1,131 +1,3 @@
-  <div id="container">
-
-<section id="panelPersonaje">
-        <div id="memeGeneratorLogo"><span>Memespecial</span><span>Generator</span></div>
-        <h1><?echo $json['usuario']['nombre']; ?></h1>
-        <div><? echo "<img src='https://apps.t2omedia.com.mx/php2/jcuervo/Avatar/".$json['usuario']['avatar_img']."' />"; ?></div>
-        <? if(Yii::app()->session['id_facebook']==$json['usuario']['id_facebook']){ ?>
-        <div id="actions"><a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/avatars/update/<?echo Yii::app()->session['usuario_id']; ?>" class="btn"><i class="icon-edit"></i> Editar</a></div>
-        <? }?>
-</section>
-
-<section id="panelContent">
-
-        <a href="<?php echo CController::CreateUrl('Comics/create'); ?>">Crea un meme nuevo</a>
-    
-        <div class="tabs"><a  href="<? echo Yii::app()->session['protocol']; ?>apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/<? echo Yii::app()->session['id_facebook']; ?>" class="mismemesmenu" >Mis Memes</a><a  id="misamigos"  class="selectedTab menu" href="">De mis amigos</a><a id="categoria" class="menu" href="">Por categoría</a></div>
-        
-
-     <?     
-
-/*
-       <div class="js-slides">
-          <div class="slides_container">
-            
-       
-
-         <div class="slide itemThumbs">
-
-          <?
-
-           if(count($comicsAmigos)!=0){
-          
-            foreach ($comicsAmigos as $key => $value) { 
-                   
-                   if($key<10){
-                   
-                    echo '<div class="itemThumbnail"><div><a data-fancybox-type="iframe" href="'.Yii::app()->session['protocol'].'apps.t2omedia.com.mx/php2/jcuervo/index.php/App/detalle/'.$value["id"].'"  id="'.$value["id"].'"  class="js-lightbox cdetail">'.CHtml::image(Yii::app()->request->baseUrl."/Comics/".$value['imagen']).'</a><div><a href="'.Yii::app()->session['protocol'].'apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/'.$value['id_facebook'].'">'.CHtml::image('https://graph.facebook.com/'.$value['id_facebook'].'/picture').'</a></div></div></div>';
-                   
-                   }
-              }
-             
-            }  
-               
-          ?>
-
-        </div>
-
-        </div>
-        <a class="prev"><i class="icon-chevron-left"></i></a><a class="next"><i class="icon-chevron-right"></i></a>
-        
-      </div>
-      */
-
-      ?>
-
-
-      
-
-<div class="js-slides">
-          <div class="slides_container">
-            <div class="slide itemThumbs">
-
-              <div class="itemThumbnail">
-                <div><a data-fancybox-type="iframe" href="detalle.html" class="js-lightbox"><img src="http://ima.gs/transparent/000000/A2A2A2/640x480.png"></a></div>
-              </div>
-              
-              <div class="itemThumbnail">
-                <div><a data-fancybox-type="iframe" href="detalle.html" class="js-lightbox"><img src="http://ima.gs/transparent/000000/A2A2A2/640x480.png"></a></div>
-              </div>
-              <div class="itemThumbnail">
-                <div><a data-fancybox-type="iframe" href="detalle.html" class="js-lightbox"><img src="http://ima.gs/transparent/000000/A2A2A2/640x480.png"></a></div>
-              </div>
-              <div class="itemThumbnail">
-                <div><a data-fancybox-type="iframe" href="detalle.html" class="js-lightbox"><img src="http://ima.gs/transparent/000000/A2A2A2/640x480.png"></a></div>
-              </div>
-              <div class="itemThumbnail">
-                <div><a data-fancybox-type="iframe" href="detalle.html" class="js-lightbox"><img src="http://ima.gs/transparent/000000/A2A2A2/640x480.png"></a></div>
-              </div>
-              <div class="itemThumbnail">
-                <div><a data-fancybox-type="iframe" href="detalle.html" class="js-lightbox"><img src="http://ima.gs/transparent/000000/A2A2A2/640x480.png"></a></div>
-              </div>
-              <div class="itemThumbnail">
-                <div><a data-fancybox-type="iframe" href="detalle.html" class="js-lightbox"><img src="http://ima.gs/transparent/000000/A2A2A2/640x480.png"></a></div>
-              </div>
-              <div class="itemThumbnail">
-                <div><a data-fancybox-type="iframe" href="detalle.html" class="js-lightbox"><img src="http://ima.gs/transparent/000000/A2A2A2/640x480.png"></a></div>
-              </div>
-              <div class="itemThumbnail">
-                <div><a data-fancybox-type="iframe" href="detalle.html" class="js-lightbox"><img src="http://ima.gs/transparent/000000/A2A2A2/640x480.png"></a></div>
-              </div>
-            </div>
-            <div class="slide itemThumbs">
-              <div class="itemThumbnail">
-                <div><a data-fancybox-type="iframe" href="detalle.html" class="js-lightbox"><img src="http://ima.gs/transparent/000000/A2A2A2/640x480.png"></a></div>
-              </div>
-              <div class="itemThumbnail">
-                <div><a data-fancybox-type="iframe" href="detalle.html" class="js-lightbox"><img src="http://ima.gs/transparent/000000/A2A2A2/640x480.png"></a></div>
-              </div>
-              <div class="itemThumbnail">
-                <div><a data-fancybox-type="iframe" href="detalle.html" class="js-lightbox"><img src="http://ima.gs/transparent/000000/A2A2A2/640x480.png"></a></div>
-              </div>
-              <div class="itemThumbnail">
-                <div><a data-fancybox-type="iframe" href="detalle.html" class="js-lightbox"><img src="http://ima.gs/transparent/000000/A2A2A2/640x480.png"></a></div>
-              </div>
-              <div class="itemThumbnail">
-                <div><a data-fancybox-type="iframe" href="detalle.html" class="js-lightbox"><img src="http://ima.gs/transparent/000000/A2A2A2/640x480.png"></a></div>
-              </div>
-              <div class="itemThumbnail">
-                <div><a data-fancybox-type="iframe" href="detalle.html" class="js-lightbox"><img src="http://ima.gs/transparent/000000/A2A2A2/640x480.png"></a></div>
-              </div>
-              <div class="itemThumbnail">
-                <div><a data-fancybox-type="iframe" href="detalle.html" class="js-lightbox"><img src="http://ima.gs/transparent/000000/A2A2A2/640x480.png"></a></div>
-              </div>
-              <div class="itemThumbnail">
-                <div><a data-fancybox-type="iframe" href="detalle.html" class="js-lightbox"><img src="http://ima.gs/transparent/000000/A2A2A2/640x480.png"></a></div>
-              </div>
-              <div class="itemThumbnail">
-                <div><a data-fancybox-type="iframe" href="detalle.html" class="js-lightbox"><img src="http://ima.gs/transparent/000000/A2A2A2/640x480.png"></a></div>
-              </div>
-            </div>
-          </div><a class="prev"><i class="icon-chevron-left"></i></a><a class="next"><i class="icon-chevron-right"></i></a>
-        </div>
-
-</section>
-</div>
-
-
-<? /*
 <div id="container">
 
 <section id="panelPersonaje">
@@ -153,7 +25,6 @@
      <? }?>
 
 
-<? 
 <div class="js-slides">
     <div class="slides_container">        
        <div class="slide itemThumbs"> 
@@ -172,11 +43,8 @@
       </div>
 </div>
 
-?>
 
 
 
 </section>
 </div>
-
-*/ ?>
