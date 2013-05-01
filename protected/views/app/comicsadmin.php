dbb51c76089f3ce9b5d64face12010d0a34c7c86
Oscar <oscarolbe@hotmail.com>
2013-05-01 18:02:49 -0500

    a

--- protected/views/app/comicsadmin.php
+++ protected/views/app/comicsadmin.php
@@ -13,7 +13,7 @@
 	'columns'=>array(
 		array(
             'header' => 'Imagen', 
-	        'value'=>'  CHtml::link(CHtml::image(Yii::app()->request->baseUrl."/Comics/".$data->imagen), array("items/viewslug", "slug"=>$data->imagen)) ',
+	        'value'=>'  CHtml::link(CHtml::image(Yii::app()->request->baseUrl."/Comics/".$data->imagen),Yii::app()->request->baseUrl."/Comics/".$data->imagen) ',
 	        'type'=>'raw',
         ),
 		'imagen',
