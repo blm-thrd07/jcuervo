643391a0b18f17a370c00fc4c2625f374b6853a0
Oscar <oscarolbe@hotmail.com>
2013-05-01 17:55:58 -0500

    link image admin

--- protected/views/app/usuariosadmin.php
+++ protected/views/app/usuariosadmin.php
@@ -4,7 +4,9 @@
 <a href="<?php echo CController::createUrl('app/admin'); ?>">Regresar</a>
 <div id="myerrordiv"></div>
 <?php
+//$image = CHtml::image($imageUrl, $data->name, array('class' => 'deals_product_image'));
 
+//echo CHtml::link($image, array('items/viewslug', 'slug'=>$data->slug));
 
 $this->widget('zii.widgets.grid.CGridView', array(
 	'id'=>'admin-usuarios-grid',
@@ -12,9 +14,15 @@ $this->widget('zii.widgets.grid.CGridView', array(
 	'filter'=>$model,
 	'ajaxUpdateError'=>'function(xhr,ts,et,err){ $("#myerrordiv").text(err); }',
 	'columns'=>array(
-		'id',
+		array(
+            'header' => 'Facebook', 
+	        'value'=>'  CHtml::link(CHtml::image("https://graph.facebook.com/".$data->id_facebook."/picture"), array("items/viewslug", "slug"=>$data->slug)) ',
+	        'type'=>'raw',
+	        //'htmlOptions'=>array('width'=>30),
+        ),
 		'correo',
 		'nombre',
+		'idFb',
 		array(
             'header' => 'es Fan?',
             'name' => 'isFan',
