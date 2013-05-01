72e86a2b0da794357a967d6f48f81e8a965e165d
Oscar <oscarolbe@hotmail.com>
2013-05-01 14:56:53 -0500

    id mi avatar

--- protected/views/comics/create.php
+++ protected/views/comics/create.php
@@ -562,6 +562,7 @@ Yii::app()->getClientScript()->registerScript('registrar', '
     return false;
   });
  
+ confMiAvatar.attrs.id='.$id_miavatar.';
  insertar("amigo",url_miavatar,confMiAvatar);
 
     
