fd710882ac0c053ef4f36379edc67d68def80060
Oscar <oscarolbe@hotmail.com>
2013-05-03 09:43:53 -0500

    h

--- protected/views/avatars/update.php
+++ protected/views/avatars/update.php
@@ -438,7 +438,7 @@ Yii::app()->getClientScript()->registerScript('registrar', '
     var layerfondo = new Kinetic.Layer()
     imageFondo = new Image();
     imageFondo.onload = function(){
-      var fondo = new Kinetic.Image({ x: 190,y: 141,height: 448,width: 250, image:imageFondo, rotation: 0, offset: [190, 140] })
+      var fondo = new Kinetic.Image({ x: 190,y: 139,height: 448,width: 250, image:imageFondo, rotation: 0, offset: [190, 140] })
       layerfondo.add(fondo);
       layerfondo.draw();
       layerPersonaje.moveToTop();
