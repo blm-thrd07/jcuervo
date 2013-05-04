c2ab6a3908fe7c00616c0cff93f0cbe6fe1f1aed
Oscar <oscarolbe@hotmail.com>
2013-05-04 14:17:08 -0500

    [1,1]

--- protected/views/comics/create.php
+++ protected/views/comics/create.php
@@ -127,7 +127,6 @@ Yii::app()->getClientScript()->registerScript('registrar', '
   currentSelected = null;
   currentText = null;
   var amigos=[];
-  var scale = 1;
   var scaleUpFactor= 1.05, scaleDownFactor=0.95;
   var trans = null;
   stageComic = new Kinetic.Stage({
@@ -158,10 +157,10 @@ Yii::app()->getClientScript()->registerScript('registrar', '
   halfx = stageComic.getWidth() / 2;
   halfy = stageComic.getHeight() / 2;
 
-  confAvatar = { x: halfx,y: halfy,height: 230,width: 128,draggable: true,offset: [64, 115],startScale: scale,name: "amigo",tipo: "amigo", mirror:1, dragBoundFunc: function(pos) { X=pos.x; Y=pos.y; dy = ((stageHeight-53)-(this.attrs.height/2)); dy2 = (this.attrs.height/2)+12; dx = (this.attrs.width/2)+12; dx2 = ((stageWidth-12)-(this.attrs.width/2)); if(X<dx){X=dx} if(X>dx2){X=dx2;} if(Y<dy2){Y=dy2;} if(Y>dy){Y=dy;} return({x:X, y:Y}); }};
-  confObjeto = {x: halfx,y: halfy,height: 100,width: 100,draggable: true,offset: [50, 50],startScale: scale,name: "objeto",tipo: "objeto", mirror:1, id:2, dragBoundFunc: function(pos) { X=pos.x; Y=pos.y; dy = ((stageHeight-53)-(this.attrs.height/2)); dy2 = (this.attrs.height/2)+12; dx = (this.attrs.width/2)+12; dx2 = ((stageWidth-12)-(this.attrs.width/2)); if(X<dx){X=dx} if(X>dx2){X=dx2;} if(Y<dy2){Y=dy2;} if(Y>dy){Y=dy;} return({x:X, y:Y}); }};
-  confBackground = {x: 190,y: 140,rotation: 0,height: 383,width: 510,image: imageBackground,offset: [190, 140],startScale: scale,name: "fondo",id: 1};
-  confMiAvatar = { x: halfx,y: halfy,height: 230,width: 128,draggable: true,offset: [64,115],startScale: scale,name: "MiAvatar",tipo: "amigo", mirror:1, id:"'.$id_miavatar.'", dragBoundFunc: function(pos) { X=pos.x; Y=pos.y; dy = ((stageHeight-53)-(this.attrs.height/2)); dy2 = (this.attrs.height/2)+12; dx = (this.attrs.width/2)+12; dx2 = ((stageWidth-12)-(this.attrs.width/2)); if(X<dx){X=dx} if(X>dx2){X=dx2;} if(Y<dy2){Y=dy2;} if(Y>dy){Y=dy;} return({x:X, y:Y}); }};
+  confAvatar = { x: halfx,y: halfy,height: 230,width: 128,draggable: true,offset: [64, 115],startScale: [1,1],name: "amigo",tipo: "amigo", mirror:1, dragBoundFunc: function(pos) { X=pos.x; Y=pos.y; dy = ((stageHeight-53)-(this.attrs.height/2)); dy2 = (this.attrs.height/2)+12; dx = (this.attrs.width/2)+12; dx2 = ((stageWidth-12)-(this.attrs.width/2)); if(X<dx){X=dx} if(X>dx2){X=dx2;} if(Y<dy2){Y=dy2;} if(Y>dy){Y=dy;} return({x:X, y:Y}); }};
+  confObjeto = {x: halfx,y: halfy,height: 100,width: 100,draggable: true,offset: [50, 50],startScale: [1,1],name: "objeto",tipo: "objeto", mirror:1, id:2, dragBoundFunc: function(pos) { X=pos.x; Y=pos.y; dy = ((stageHeight-53)-(this.attrs.height/2)); dy2 = (this.attrs.height/2)+12; dx = (this.attrs.width/2)+12; dx2 = ((stageWidth-12)-(this.attrs.width/2)); if(X<dx){X=dx} if(X>dx2){X=dx2;} if(Y<dy2){Y=dy2;} if(Y>dy){Y=dy;} return({x:X, y:Y}); }};
+  confBackground = {x: 190,y: 140,rotation: 0,height: 383,width: 510,image: imageBackground,offset: [190, 140],startScale: [1,1],name: "fondo",id: 1};
+  confMiAvatar = { x: halfx,y: halfy,height: 230,width: 128,draggable: true,offset: [64,115],startScale: [1,1],name: "MiAvatar",tipo: "amigo", mirror:1, id:"'.$id_miavatar.'", dragBoundFunc: function(pos) { X=pos.x; Y=pos.y; dy = ((stageHeight-53)-(this.attrs.height/2)); dy2 = (this.attrs.height/2)+12; dx = (this.attrs.width/2)+12; dx2 = ((stageWidth-12)-(this.attrs.width/2)); if(X<dx){X=dx} if(X>dx2){X=dx2;} if(Y<dy2){Y=dy2;} if(Y>dy){Y=dy;} return({x:X, y:Y}); }};
 
   layerFondo = new Kinetic.Layer();
   layerComic = new Kinetic.Layer();
@@ -364,7 +363,8 @@ Yii::app()->getClientScript()->registerScript('registrar', '
           if (trans) {
             trans.stop();
           }
-          this.attrs.startScale = this.attrs.scale.x;
+          this.attrs.startScale.x = this.attrs.scale.x;
+          this.attrs.startScale.y = this.attrs.scale.y;
           return this.setAttrs({
             scale: {
               x: this.attrs.scale.x * scaleUpFactor,
@@ -381,8 +381,8 @@ Yii::app()->getClientScript()->registerScript('registrar', '
             duration: 0.5,
             easing: "elastic-ease-out",
             scale: {
-              x: this.attrs.startScale*this.attrs.mirror,
-              y: -1*this.attrs.startScale
+              x: this.attrs.startScale.x,
+              y: this.attrs.startScale.y
             }
           });
           if( (e.clientX-rect.left) < 0 || (e.clientX-rect.left) > stageComic.getWidth() || (e.clientY-rect.top) < 0 || (e.clientY-rect.top) > stageComic.getHeight() )
