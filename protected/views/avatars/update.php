ae6d729999d3356108967937fe214e7fc501b2bb
Oscar <oscarolbe@hotmail.com>
2013-05-04 10:49:14 -0500

    a

--- protected/views/avatars/update.php
+++ protected/views/avatars/update.php
@@ -465,7 +465,7 @@ Yii::app()->getClientScript()->registerScript('registrar', '
             url: BaseUrl+"/index.php/avatars/UpdateImg",
             data: avatarJson,
             success: function(url){
-              window.location=url;
+              //window.location=url;
               $("#overlay").css("display","none"); 
               $("#popup").css("display","none"); 
             },
