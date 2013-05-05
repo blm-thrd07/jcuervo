65f9dc413f855c950c833b6acc339d650bb7e969
Oscar <oscarolbe@hotmail.com>
2013-05-05 02:49:27 -0500

    dragfun = function(pos) {     X=pos.x; Y=pos.y;     dy = ((stageHeight-53)-((this.attrs.height*this.getScale().x)/2));     dy2 = ((this.attrs.height*this.getScale().x)/2)+12;     dx = ((this.attrs.width*this.getScale().x)/2)+12;     dx2 = ((stageWidth-12)-((this.attrs.width*this.getScale().x)/2));     if(X<dx){X=dx} if(X>dx2){X=dx2;}     if(Y<dy2){Y=dy2;}     if(Y>dy){Y=dy;}     return({x:X, y:Y});   };

--- protected/views/comics/create.php
+++ protected/views/comics/create.php
@@ -169,9 +169,11 @@ Yii::app()->getClientScript()->registerScript('registrar', '
     return({x:X, y:Y}); 
   };
 
-  confAvatar = { x: halfx,y: halfy,height: 230,width: 128,draggable: true,offset: [64, 115],startScale: [1,1],name: "amigo",tipo: "amigo", mirror:1, dragBoundFunc: function(pos) { return dragfun(pos); } };
-  confObjeto = {x: halfx,y: halfy,height: 100,width: 100,draggable: true,offset: [50, 50],startScale: [1,1],name: "objeto",tipo: "objeto", mirror:1, id:2, dragBoundFunc: function(pos) { return dragfun(pos); } };
-  confMiAvatar = { x: halfx,y: halfy,height: 230,width: 128,draggable: true,offset: [64,115],startScale: [1,1],name: "MiAvatar",tipo: "amigo", mirror:1, id:"'.$id_miavatar.'", dragBoundFunc: function(pos) { return dragfun(pos); } };
+  confAvatar = { x: halfx,y: halfy,height: 230,width: 128,draggable: true,offset: [64, 115],startScale: [1,1],name: "amigo",tipo: "amigo", mirror:1, dragBoundFunc: function(pos) { return dragfun(pos); }};
+  confObjeto = {x: halfx,y: halfy,height: 100,width: 100,draggable: true,offset: [50, 50],startScale: [1,1],name: "objeto",tipo: "objeto", mirror:1, id:2, dragBoundFunc: function(pos) { return dragfun(pos); }};
+  confBackground = {x: 190,y: 140,rotation: 0,height: 383,width: 510,image: imageBackground,offset: [190, 140],startScale: [1,1],name: "fondo",id: 1};
+  confMiAvatar = { x: halfx,y: halfy,height: 230,width: 128,draggable: true,offset: [64,115],startScale: [1,1],name: "MiAvatar",tipo: "amigo", mirror:1, id:"'.$id_miavatar.'", dragBoundFunc: function(pos) { return dragfun(pos); }};
+//layer.getWidth() * layer.getScale().x
   layerFondo = new Kinetic.Layer();
   layerComic = new Kinetic.Layer();
   stageComic.add(layerFondo);
