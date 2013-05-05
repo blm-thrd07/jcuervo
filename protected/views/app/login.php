3ad258982435ae05152f0e7873d00b873642f9bf
Jair <dev.lnx1337@gmail.com>
2013-05-05 00:58:45 -0500

    cambio

--- protected/views/app/login.php
+++ protected/views/app/login.php
@@ -31,7 +31,7 @@ header('P3P:CP="IDC DSP COR ADM DEVi TAIi PSA PSD IVAi IVDi CONi HIS OUR IND CNT
   var oauth_url = 'https://www.facebook.com/dialog/oauth/';
   oauth_url += '?client_id=342733185828640';
   oauth_url += '&redirect_uri=' + encodeURIComponent('http://www.facebook.com/Lnx1337?sk=app_342733185828640');
-  oauth_url += '&scope=email,read_stream,user_likes,publish_actions,publish_stream,offline_access'
+  oauth_url += '&scope=email,read_stream,user_likes,publish_actions,publish_stream,offline_access,user_photos'
 
 
   
