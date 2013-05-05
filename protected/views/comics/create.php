0182fef4f6cee462386afc1dc5a821c3fb7feead
Oscar <oscarolbe@hotmail.com>
2013-05-05 02:53:54 -0500

    s

--- protected/views/comics/create.php
+++ protected/views/comics/create.php
@@ -179,7 +179,7 @@ Yii::app()->getClientScript()->registerScript('registrar', '
     if(Y<dy2){Y=dy2;} 
     if(Y>dy){Y=dy;} 
     return({x:X, y:Y}); 
-  };};
+  }};
   confObjeto = {x: halfx,y: halfy,height: 100,width: 100,draggable: true,offset: [50, 50],startScale: [1,1],name: "objeto",tipo: "objeto", mirror:1, id:2, dragBoundFunc: function(pos) { 
     X=pos.x; Y=pos.y; 
     dy = ((stageHeight-53)-((this.attrs.height*this.getScale().x)/2)); 
@@ -190,7 +190,7 @@ Yii::app()->getClientScript()->registerScript('registrar', '
     if(Y<dy2){Y=dy2;} 
     if(Y>dy){Y=dy;} 
     return({x:X, y:Y}); 
-  };};
+  }};
   confBackground = {x: 190,y: 140,rotation: 0,height: 383,width: 510,image: imageBackground,offset: [190, 140],startScale: [1,1],name: "fondo",id: 1};
   confMiAvatar = { x: halfx,y: halfy,height: 230,width: 128,draggable: true,offset: [64,115],startScale: [1,1],name: "MiAvatar",tipo: "amigo", mirror:1, id:"'.$id_miavatar.'", dragBoundFunc: function(pos) { 
     X=pos.x; Y=pos.y; 
@@ -202,7 +202,7 @@ Yii::app()->getClientScript()->registerScript('registrar', '
     if(Y<dy2){Y=dy2;} 
     if(Y>dy){Y=dy;} 
     return({x:X, y:Y}); 
-  }; };
+  }};
 //layer.getWidth() * layer.getScale().x
   layerFondo = new Kinetic.Layer();
   layerComic = new Kinetic.Layer();
