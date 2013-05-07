124893f18bbdb5bf3dde60f3cb7060fd4722cdf0
Oscar <oscarolbe@hotmail.com>
2013-05-06 19:53:48 -0500

    a

--- protected/views/comics/create.php
+++ protected/views/comics/create.php
@@ -173,20 +173,30 @@ Yii::app()->getClientScript()->registerScript('registrar', '
   confObjeto = {x: halfx,y: halfy,height: 100,width: 100,draggable: true,offset: [50, 50],startScale: [1,1],name: "objeto",tipo: "objeto", mirror:1, id:2};
   confBackground = {x: 190,y: 140,rotation: 0,height: 383,width: 510,image: imageBackground,offset: [190, 140],startScale: [1,1],name: "fondo",id: 1};
   confMiAvatar = { x: halfx,y: halfy,height: 230,width: 128,draggable: true,offset: [64,115],startScale: [1,1],name: "MiAvatar",tipo: "amigo", mirror:1, id:"'.$id_miavatar.'"};
-  //layerFondo = new Kinetic.Layer();
+  layerFondo = new Kinetic.Layer();
   layerComic = new Kinetic.Layer();
-  //stageComic.add(layerFondo);
+  stageComic.add(layerFondo);
   stageComic.add(layerComic);
 
   imageBackground = new Image();
   confBackground.image = imageBackground;
   imageBackground.onload = function(){
     fondo = new Kinetic.Image(confBackground);
-    layerComic.add(fondo);
-    layerComic.draw();
+    layerFondo.add(fondo);
+    layerFondo.draw();
   }
   imageBackground.src=BaseUrl+"/images/backgrounds/default.jpg";
-  layerComic.draw();
+  layerFondo.draw();
+
+  imageMarco = new Image();
+  confBackground.image = imageMarco;
+  imageMarco.onload = function(){
+    marco = new Kinetic.Image(confBackground);
+    layerComic.add(marco);
+    layerComic.draw();
+    layerComic.moveToTop();
+  }
+
   layerComic.getContext().globalCompositeOperation = "destination-over";
   $("#tab1 .itemMeme").on("click", function(e){ $("#textinput").attr("class", "inputClose"); var id = $(this).find("img").attr("id"); insertarFondo($(this).find("img").attr("src")); });
   $("#tab2 .itemMeme").on("click", function(e){ $("#textinput").attr("class", "inputClose"); insertar("objeto",$(this).find("img").attr("src"),confObjeto); });
