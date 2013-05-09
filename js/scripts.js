640c9c1ffd0d5588c451205fe16cba024b17348d
Jair <dev.lnx1337@gmail.com>
2013-05-09 15:11:10 -0500

    internet explorer

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,40 @@ function referrerIsFacebookApp() {
   return false;
 }
 
+
+function getInternetExplorerVersion()
+// Returns the version of Internet Explorer or a -1
+// (indicating the use of another browser).
+{
+  var rv = -1; // Return value assumes failure.
+  if (navigator.appName == 'Microsoft Internet Explorer')
+  {
+    var ua = navigator.userAgent;
+    var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null)
+      rv = parseFloat( RegExp.$1 );
+  }
+  return rv;
+}
+function checkVersion()
+{
+  var msg = "You're not using Internet Explorer.";
+  var ver = getInternetExplorerVersion();
+
+  if ( ver > -1 )
+  {
+    if ( ver >= 8.0 ) 
+      msg = "Necesitas actualizar tu explorador";
+    else
+      msg = "You should upgrade your copy of Internet Explorer.";
+  }
+  alert( msg );
+}
+
+
+checkVersion();
+
+
  $(document).bind('selectstart dragstart', function(evt)
   { evt.preventDefault(); return false; });
     
