6f7b854feb42222202560fe4f86baf8bf0b3e31a
Oscar <oscarolbe@hotmail.com>
2013-05-14 19:38:41 -0500

    admin

--- protected/views/app/usuariosadmin.php
+++ protected/views/app/usuariosadmin.php
@@ -48,3 +48,10 @@ $this->widget('zii.widgets.grid.CGridView', array(
 <br><br>
 
 
+<?php $comics = UsuariosHasTblComics::model()->with('Comic')->findAll(array('condition'=>'isSpecial=true AND isHidden=0','limit'=>4)); 
+        if(count($comics)==4){
+          foreach ($comics as $key => $value) {  
+            echo '<div class="itemThumbnail"><div><a data-fancybox-type="iframe" href="'.Yii::app()->session['protocol'].'apps.t2omedia.com.mx/php2/jcuervo/index.php/App/detalle/'.$value["id"].'" class="js-lightbox">'.CHtml::image(Yii::app()->request->baseUrl."/Comics/".$value['imagen']).'</a></div></div>';        
+          }
+        }
+        ?>
