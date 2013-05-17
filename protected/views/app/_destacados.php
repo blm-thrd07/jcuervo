c2a8f05c02624511e8caabc0eca41dc6be4e88d8
Jair <dev.lnx1337@gmail.com>
2013-05-17 16:35:38 -0500

    que  ondas

--- protected/views/app/_destacados.php
+++ protected/views/app/_destacados.php
@@ -3,9 +3,16 @@
          <div class="memeThumbs"> 
          <? 
             $cantidad_resultados=count($resultado);
+
+
             for($i=0;$i<$cantidad_resultados;$i++){
+            if($value['idFb']==Yii::app()->session['id_facebook']){
                  echo '<div class="itemMeme"><a data-fancybox-type="iframe" href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/detalle/'.$value["idFb"].'"  class="js-lightbox cdetail">'.CHtml::image(Yii::app()->request->baseUrl."/Comics/".$value['imagen']).'</a><div><a href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/'.$value['idFb'].'">'.CHtml::image('https://graph.facebook.com/'.$value['idFb'].'/picture').'</a></div>
                  </div>';
+             }else{
+                echo '<div class="itemMeme"><a data-fancybox-type="iframe" href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/detalle/'.$value["idFb"].'"  class="js-lightbox">'.CHtml::image(Yii::app()->request->baseUrl."/Comics/".$value['imagen']).'</a><div><a href="http://apps.t2omedia.com.mx/php2/jcuervo/index.php/App/Profile/'.$value['idFb'].'">'.CHtml::image('https://graph.facebook.com/'.$value['idFb'].'/picture').'</a></div>
+                 </div>';
+             }
             }  
         ?>
          </div>
