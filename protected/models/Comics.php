2f92155bf8defbe53ad3f7f60663865e0ce0e3c6
Oscar <oscarolbe@hotmail.com>
2013-05-17 00:00:37 -0500

    compartidos

--- protected/models/Comics.php
+++ protected/models/Comics.php
@@ -103,7 +103,9 @@ class Comics extends CActiveRecord
 			$cmd->bindValue(":id", $id , PDO::PARAM_STR);
 			$row = $cmd->queryAll();
         	$cantidad=$row[0]['max'];
-        	echo CHtml::encode($cantidad);
+        	if($cantidad>0)
+        		echo CHtml::encode($cantidad);
+        	else echo "0";
 		}
 	}
 
