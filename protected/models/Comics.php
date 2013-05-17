cac0c7fe59deb1ee1b8a58cd02747a1e87683ca5
Oscar <oscarolbe@hotmail.com>
2013-05-16 23:51:04 -0500

    function

--- protected/models/Comics.php
+++ protected/models/Comics.php
@@ -98,11 +98,11 @@ class Comics extends CActiveRecord
 	{
 		if(Yii::app()->session['usuario_id'])
 		{
-			/*$cmd = Yii::app()->db->createCommand('select sum(NoCompartido) from tbl_usuarios_has_tbl_comics where tbl_usuarios_id=:id')
+			$cmd = Yii::app()->db->createCommand('select sum(NoCompartido) from tbl_usuarios_has_tbl_comics where tbl_usuarios_id=:id');
 			$cmd->bindValue(":id", $id , PDO::PARAM_STR);
 			$row = $cmd->queryAll();
         	$cantidad=$row[0]['max'];
-        	echo CHtml::encode($cantidad);*/
+        	echo CHtml::encode($cantidad);
 		}
 	}
 
